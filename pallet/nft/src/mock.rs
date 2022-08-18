e2d48e3e3361f26f8292aa6353879d0537a25fd9
Jason <jason.j.tulp@gmail.com>
2022-08-19 09:53:38 +1200

    Removed ParachainId from Assets_Ext mock

--- pallet/nft/src/mock.rs
+++ pallet/nft/src/mock.rs
@@ -111,7 +111,6 @@ parameter_types! {
 
 impl pallet_assets_ext::Config for Test {
 	type Event = Event;
-	type ParachainId = TestParachainId;
 	type MaxHolds = MaxHolds;
 	type MyclAssetId = MyclAssetId;
 	type PalletId = AssetsExtPalletId;
@@ -142,13 +141,13 @@ parameter_types! {
 }
 
 impl crate::Config for Test {
-	type Event = Event;
 	type DefaultListingDuration = DefaultListingDuration;
+	type Event = Event;
 	type MultiCurrency = AssetsExt;
-	type WeightInfo = ();
 	type OnTransferSubscription = MockTransferSubscriber;
-	type ParachainId = TestParachainId;
 	type PalletId = NftPalletId;
+	type ParachainId = TestParachainId;
+	type WeightInfo = ();
 }
 
 #[derive(Default)]
