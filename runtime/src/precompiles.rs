b901975a9d22f5970d7a3a752f6610d01dc3c12a
Jason <jason.j.tulp@gmail.com>
2022-08-23 14:36:48 +1200

    Adds ERC721 Precompiles

--- runtime/src/precompiles.rs
+++ runtime/src/precompiles.rs
@@ -1,8 +1,14 @@
+use frame_support::parameter_types;
 use pallet_evm_precompile_blake2::Blake2F;
 use pallet_evm_precompile_modexp::Modexp;
 use pallet_evm_precompile_sha3fips::Sha3FIPS256;
 use pallet_evm_precompile_simple::{ECRecover, ECRecoverPublicKey, Identity, Ripemd160, Sha256};
-use precompile_utils::precompile_set::*;
+use pallet_evm_precompiles_erc721::Erc721PrecompileSet;
+use precompile_utils::{constants::ERC721_PRECOMPILE_ADDRESS_PREFIX, precompile_set::*};
+
+parameter_types! {
+	pub Erc721AssetPrefix: &'static [u8] = ERC721_PRECOMPILE_ADDRESS_PREFIX;
+}
 
 /// The PrecompileSet installed in the Futureverse runtime.
 /// We include six of the nine Istanbul precompiles
@@ -31,5 +37,7 @@ pub type FutureversePrecompiles<R> = PrecompileSetBuilder<
 				// Futureverse specific precompiles:
 			),
 		>,
+		// Prefixed precompile sets (XC20)
+		PrecompileSetStartingWith<Erc721AssetPrefix, Erc721PrecompileSet<R>>,
 	),
 >;
