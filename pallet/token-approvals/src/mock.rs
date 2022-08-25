5e54968ac11564591325a9901720804d75fbc514
Jason <jason.j.tulp@gmail.com>
2022-08-25 18:48:54 +1200

    Fixed tests

--- pallet/token-approvals/src/mock.rs
+++ pallet/token-approvals/src/mock.rs
@@ -1,30 +1,30 @@
 /* Copyright 2019-2021 Centrality Investments Limited
-*
-* Licensed under the LGPL, Version 3.0 (the "License");
-* you may not use this file except in compliance with the License.
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-* You may obtain a copy of the License at the root of this project source code,
-* or at:
-*     https://centrality.ai/licenses/gplv3.txt
-*     https://centrality.ai/licenses/lgplv3.txt
-*/
+ *
+ * Licensed under the LGPL, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * You may obtain a copy of the License at the root of this project source code,
+ * or at:
+ *     https://centrality.ai/licenses/gplv3.txt
+ *     https://centrality.ai/licenses/lgplv3.txt
+ */
 
 use crate as token_approvals;
-use cennznet_primitives::types::{AccountId, AssetId, Balance, TokenId};
-use crml_generic_asset::impls::TransferDustImbalance;
-use crml_support::IsTokenOwner;
 use frame_support::{parameter_types, PalletId};
-use hex_literal::hex;
+use frame_system::EnsureRoot;
+use seed_pallet_common::IsTokenOwner;
+use seed_primitives::{AssetId, Balance, TokenId};
 use sp_core::H256;
 use sp_runtime::{
 	testing::Header,
 	traits::{BlakeTwo256, IdentityLookup},
 };
 
+pub type AccountId = u64;
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
 
@@ -35,9 +35,11 @@ frame_support::construct_runtime!(
 		UncheckedExtrinsic = UncheckedExtrinsic,
 	{
 		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
-		GenericAsset: crml_generic_asset::{Pallet, Call, Storage, Config<T>, Event<T>},
-		Nft: crml_nft::{Pallet, Call, Storage, Event<T>},
-		TokenApprovals: token_approvals::{Pallet, Call, Storage},
+		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+		Assets: pallet_assets::{Pallet, Storage, Config<T>, Event<T>},
+		AssetsExt: pallet_assets_ext::{Pallet, Storage, Event<T>},
+		Nft: pallet_nft::{Pallet, Call, Storage, Event<T>},
+		TokenApprovals: token_approvals::{Pallet, Call, Storage, Event<T>},
 	}
 );
 
@@ -62,7 +64,7 @@ impl frame_system::Config for Test {
 	type DbWeight = ();
 	type Version = ();
 	type PalletInfo = PalletInfo;
-	type AccountData = ();
+	type AccountData = pallet_balances::AccountData<Balance>;
 	type OnNewAccount = ();
 	type OnKilledAccount = ();
 	type SystemWeightInfo = ();
@@ -72,56 +74,100 @@ impl frame_system::Config for Test {
 }
 
 parameter_types! {
-	pub const TreasuryPalletId: PalletId = PalletId(*b"py/trsry");
+	pub const AssetDeposit: Balance = 1_000_000;
+	pub const AssetAccountDeposit: Balance = 16;
+	pub const ApprovalDeposit: Balance = 1;
+	pub const AssetsStringLimit: u32 = 50;
+	pub const MetadataDepositBase: Balance = 1 * 68;
+	pub const MetadataDepositPerByte: Balance = 1;
 }
-impl crml_generic_asset::Config for Test {
-	type AssetId = AssetId;
-	type Balance = Balance;
+pub type AssetsForceOrigin = EnsureRoot<AccountId>;
+
+impl pallet_assets::Config for Test {
 	type Event = Event;
-	type OnDustImbalance = TransferDustImbalance<TreasuryPalletId>;
+	type Balance = Balance;
+	type AssetId = AssetId;
+	type Currency = Balances;
+	type ForceOrigin = AssetsForceOrigin;
+	type AssetDeposit = AssetDeposit;
+	type MetadataDepositBase = MetadataDepositBase;
+	type MetadataDepositPerByte = MetadataDepositPerByte;
+	type ApprovalDeposit = ApprovalDeposit;
+	type StringLimit = AssetsStringLimit;
+	type Freezer = ();
+	type Extra = ();
 	type WeightInfo = ();
+	type AssetAccountDeposit = AssetAccountDeposit;
 }
 
 parameter_types! {
+	pub const MyclAssetId: AssetId = 1;
+	pub const AssetsExtPalletId: PalletId = PalletId(*b"assetext");
+	pub const MaxHolds: u32 = 16;
+	pub const MaxReserves: u32 = 50;
+}
+
+impl pallet_assets_ext::Config for Test {
+	type Event = Event;
+	type ParachainId = TestParachainId;
+	type MaxHolds = MaxHolds;
+	type MyclAssetId = MyclAssetId;
+	type PalletId = AssetsExtPalletId;
+}
+
+impl pallet_balances::Config for Test {
+	type Balance = Balance;
+	type Event = Event;
+	type DustRemoval = ();
+	type ExistentialDeposit = ();
+	type AccountStore = System;
+	type MaxLocks = ();
+	type WeightInfo = ();
+	type MaxReserves = MaxReserves;
+	type ReserveIdentifier = [u8; 8];
+}
+
+parameter_types! {
+	pub const NftPalletId: PalletId = PalletId(*b"nftokens");
 	pub const DefaultListingDuration: u64 = 5;
 	pub const MaxAttributeLength: u8 = 140;
+	pub const TestParachainId: u32 = 100;
 }
-impl crml_nft::Config for Test {
-	type Event = Event;
-	type MultiCurrency = GenericAsset;
-	type MaxAttributeLength = MaxAttributeLength;
+impl pallet_nft::Config for Test {
 	type DefaultListingDuration = DefaultListingDuration;
-	type WeightInfo = ();
+	type Event = Event;
+	type MultiCurrency = AssetsExt;
 	type OnTransferSubscription = TokenApprovals;
+	type PalletId = NftPalletId;
+	type ParachainId = TestParachainId;
+	type WeightInfo = ();
 }
 
 pub struct MockTokenOwner;
 impl IsTokenOwner for MockTokenOwner {
 	type AccountId = AccountId;
 
-	fn check_ownership(account: &Self::AccountId, token_id: &TokenId) -> bool {
-		let test_account = AccountId::from(hex!("63766d3a00000000000000a86e122edbdcba4bf24a2abf89f5c230b37df49d4a"));
-		if account == &test_account && token_id == &(0u32, 0u32, 0u32) {
-			return true;
+	fn is_owner(account: &Self::AccountId, token_id: &TokenId) -> bool {
+		let test_account: AccountId = 10;
+		if account == &test_account && token_id == &(0u32, 0u32) {
+			return true
 		}
-		return false;
+		return false
 	}
 }
 
 impl crate::Config for Test {
-	type MultiCurrency = GenericAsset;
+	type Event = Event;
 	type IsTokenOwner = MockTokenOwner;
 }
 
 #[derive(Default)]
-pub struct ExtBuilder;
+pub struct TestExt;
 
-impl ExtBuilder {
+impl TestExt {
 	pub fn build(self) -> sp_io::TestExternalities {
-		let mut ext: sp_io::TestExternalities = frame_system::GenesisConfig::default()
-			.build_storage::<Test>()
-			.unwrap()
-			.into();
+		let mut ext: sp_io::TestExternalities =
+			frame_system::GenesisConfig::default().build_storage::<Test>().unwrap().into();
 
 		ext.execute_with(|| {
 			System::initialize(&1, &[0u8; 32].into(), &Default::default());
