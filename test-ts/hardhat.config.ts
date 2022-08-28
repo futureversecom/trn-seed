baf9b304dbb2fae8390b5151c6e61c446bcfeab0
zees-dev <dev.786zshan@gmail.com>
2022-08-29 11:23:33 +1200

    init e2e tests

---
+++ test-ts/hardhat.config.ts
@@ -0,0 +1,46 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import { utils } from "ethers";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.14",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      gasPrice: utils.parseUnits("100", "gwei").toNumber(),
+    },
+    seed: {
+      url: 'http://localhost:9933',
+      accounts: [`0xcb6df9de1efca7a3998a8ead4e02159d5fa99c3e0d4fd6432667390bb4726854`],
+      // accounts: process.env.MAINNET_PRIVATE_KEY !== undefined ? [process.env.MAINNET_PRIVATE_KEY] : [],
+      // gasPrice: utils.parseUnits("50", "gwei").toNumber(),
+      chainId: 3999,
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
---
+++ test-ts/test/ERC20Precompile.test.ts
@@ -0,0 +1,144 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+import { ApiPromise, HttpProvider, WsProvider } from '@polkadot/api';
+import { Keyring } from '@polkadot/keyring';
+import { u8aToHex, stringToHex, hexToU8a } from '@polkadot/util';
+
+const typedefs = {
+  AccountId: 'EthereumAccountId',
+  AccountId20: 'EthereumAccountId',
+  AccountId32: 'EthereumAccountId',
+  Address: 'AccountId',
+  LookupSource: 'AccountId',
+  Lookup0: 'AccountId',
+  EthereumSignature: {
+    r: 'H256',
+    s: 'H256',
+    v: 'U8'
+  },
+  ExtrinsicSignature: 'EthereumSignature',
+  SessionKeys: '([u8; 32], [u8; 32])'
+};
+
+describe("ERC20 Precompile", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function deployOneYearLockFixture() {
+    const wsProvider = new WsProvider('ws://localhost:9944');
+    const api = await ApiPromise.create({ provider: wsProvider, types: typedefs });
+
+    const keyring = new Keyring({ type: 'ethereum' });
+    const ALICE = keyring
+      .addFromSeed(hexToU8a('0xcb6df9de1efca7a3998a8ead4e02159d5fa99c3e0d4fd6432667390bb4726854'), { name: 'Alice default' });
+
+    let txHash = await api.tx.assetsExt.createAsset().signAndSend(ALICE);
+    console.log(txHash);
+
+    // TODO - get asset id from last event and save it in a variable
+
+    console.log(api.genesisHash.toHex());
+  }
+
+  describe("Deployment", function () {
+    it("Should set the right unlockTime", async function () {
+      await loadFixture(deployOneYearLockFixture);
+
+      expect(1).to.equal(1);
+    });
+
+    // it("Should set the right owner", async function () {
+    //   const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+
+    //   expect(await lock.owner()).to.equal(owner.address);
+    // });
+
+    // it("Should receive and store the funds to lock", async function () {
+    //   const { lock, lockedAmount } = await loadFixture(
+    //     deployOneYearLockFixture
+    //   );
+
+    //   expect(await ethers.provider.getBalance(lock.address)).to.equal(
+    //     lockedAmount
+    //   );
+    // });
+
+    // it("Should fail if the unlockTime is not in the future", async function () {
+    //   // We don't use the fixture here because we want a different deployment
+    //   const latestTime = await time.latest();
+    //   const Lock = await ethers.getContractFactory("Lock");
+    //   await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
+    //     "Unlock time should be in the future"
+    //   );
+    // });
+  });
+
+  // describe("Withdrawals", function () {
+  //   describe("Validations", function () {
+  //     it("Should revert with the right error if called too soon", async function () {
+  //       const { lock } = await loadFixture(deployOneYearLockFixture);
+
+  //       await expect(lock.withdraw()).to.be.revertedWith(
+  //         "You can't withdraw yet"
+  //       );
+  //     });
+
+  //     it("Should revert with the right error if called from another account", async function () {
+  //       const { lock, unlockTime, otherAccount } = await loadFixture(
+  //         deployOneYearLockFixture
+  //       );
+
+  //       // We can increase the time in Hardhat Network
+  //       await time.increaseTo(unlockTime);
+
+  //       // We use lock.connect() to send a transaction from another account
+  //       await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
+  //         "You aren't the owner"
+  //       );
+  //     });
+
+  //     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
+  //       const { lock, unlockTime } = await loadFixture(
+  //         deployOneYearLockFixture
+  //       );
+
+  //       // Transactions are sent using the first signer by default
+  //       await time.increaseTo(unlockTime);
+
+  //       await expect(lock.withdraw()).not.to.be.reverted;
+  //     });
+  //   });
+
+  //   describe("Events", function () {
+  //     it("Should emit an event on withdrawals", async function () {
+  //       const { lock, unlockTime, lockedAmount } = await loadFixture(
+  //         deployOneYearLockFixture
+  //       );
+
+  //       await time.increaseTo(unlockTime);
+
+  //       await expect(lock.withdraw())
+  //         .to.emit(lock, "Withdrawal")
+  //         .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
+  //     });
+  //   });
+
+  //   describe("Transfers", function () {
+  //     it("Should transfer the funds to the owner", async function () {
+  //       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
+  //         deployOneYearLockFixture
+  //       );
+
+  //       await time.increaseTo(unlockTime);
+
+  //       await expect(lock.withdraw()).to.changeEtherBalances(
+  //         [owner, lock],
+  //         [lockedAmount, -lockedAmount]
+  //       );
+  //     });
+  //   });
+  // });
+});
