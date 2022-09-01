4c1380cb38e11cbf43f5ecf492b63cff4d706f16
“palletd” <suman.inster@gmail.com>
2022-08-31 21:31:44 -0600

    xcm removed , syn bumped

--- evm-precompiles/utils/src/data/mod.rs
+++ evm-precompiles/utils/src/data/mod.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
 
-pub mod xcm;
 pub use affix::paste;
 
 use crate::revert::{InjectBacktrace, MayRevert, RevertReason};
