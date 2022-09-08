97b95eb1adb3be2d2db62c5e33a1a64f82f9c529
Jason <jason.j.tulp@gmail.com>
2022-09-09 10:24:54 +1200

    Rename root and add comment

--- test-ts/hardhat.config.ts
+++ test-ts/hardhat.config.ts
@@ -28,6 +28,7 @@ const config: HardhatUserConfig = {
     },
     seed: {
       url: 'http://localhost:9933',
+      // Alice account
       accounts: [`0xcb6df9de1efca7a3998a8ead4e02159d5fa99c3e0d4fd6432667390bb4726854`],
       chainId: 3999,
     },
