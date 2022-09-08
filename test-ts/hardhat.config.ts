79c7c823a937a0c5504c04fb9bc44e09f5d1a2f9
JasonT <50426640+JasonTulp@users.noreply.github.com>
2022-09-09 11:03:43 +1200

    ERC721 e2e tests (#57)
    Adds ERC721 e2e tests

--- test-ts/hardhat.config.ts
+++ test-ts/hardhat.config.ts
@@ -28,8 +28,8 @@ const config: HardhatUserConfig = {
     },
     seed: {
       url: 'http://localhost:9933',
-      // Alice account
-      accounts: [`0xcb6df9de1efca7a3998a8ead4e02159d5fa99c3e0d4fd6432667390bb4726854`],
+      // Alice, Bob
+      accounts: [`0xcb6df9de1efca7a3998a8ead4e02159d5fa99c3e0d4fd6432667390bb4726854`, `0x79c3b7fc0b7697b9414cb87adcb37317d1cab32818ae18c0e97ad76395d1fdcf`],
       chainId: 3999,
     },
   },
