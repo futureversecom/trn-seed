a804b7fb86920b747fcc87cb5f81f7a7b2747133
zees-dev <dev.786zshan@gmail.com>
2022-09-29 11:25:29 +1300

    runner logic reverted

--- runtime/src/runner.rs
+++ runtime/src/runner.rs
@@ -185,11 +185,10 @@ where
 		// These values may change if we are using the fee_preferences precompile
 		let mut input = input;
 		let mut target = target;
-		let mut gas_limit = gas_limit;
 
 		// Check if we are calling with fee preferences
 		if target == H160::from_low_u64_be(FEE_PROXY_ADDRESS) {
-			let (gas_price, weight) = T::FeeCalculator::min_gas_price();
+			let (_, weight) = T::FeeCalculator::min_gas_price();
 
 			let (payment_asset_id, max_payment, new_target, new_input) = Self::decode_input(input)
 				.map_err(|err| RunnerError { error: err.into(), weight })?;
@@ -198,11 +197,9 @@ where
 			input = new_input;
 			target = new_target;
 
-			// TODO: remove `calculate_total_gas` function once we re-visit this functionality
-			// With the `calculate_total_gas`, the total_fee calc fails when user sends legacy tx with `0` gas_price.
-			// let total_fee = Self::calculate_total_gas(gas_limit, max_fee_per_gas,
-			// is_transactional) 	.map_err(|err| RunnerError { error: err.into(), weight })?;
-			let total_fee = U256::from(gas_limit) * gas_price;
+			// let total_fee = U256::from(gas_limit) * gas_price;
+			let total_fee = Self::calculate_total_gas(gas_limit, max_fee_per_gas, is_transactional)
+				.map_err(|err| RunnerError { error: err.into(), weight })?;
 
 			let native_asset_id = <T as pallet_assets_ext::Config>::NativeAssetId::get();
 			let decimals = <pallet_assets_ext::Pallet<T> as InspectMetadata<AccountId>>::decimals(
