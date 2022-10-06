c5162838bb4aa33f650453c52afef4e6ab9444d5
Jordan <beauchjord@gmail.com>
2022-10-06 16:31:26 +1300

    xrpl ethy validator whitelist (#130)
    * Wire up XRPL validator whitelist with ethy-gadget

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -65,8 +65,8 @@ pub mod keys {
 	pub use super::{BabeId, EthBridgeId, GrandpaId, ImOnlineId};
 }
 pub use seed_primitives::{
-	ethy::crypto::AuthorityId as EthBridgeId, AccountId, Address, AssetId, BabeId, Balance,
-	BlockNumber, Hash, Index, Signature,
+	ethy::{crypto::AuthorityId as EthBridgeId, ValidatorSet},
+	AccountId, Address, AssetId, BabeId, Balance, BlockNumber, Hash, Index, Signature,
 };
 
 mod bag_thresholds;
@@ -1263,8 +1263,16 @@ impl_runtime_apis! {
 	}
 
 	impl seed_primitives::ethy::EthyApi<Block> for Runtime {
-		fn validator_set() -> seed_primitives::ethy::ValidatorSet<EthBridgeId> {
+		fn validator_set() -> ValidatorSet<EthBridgeId> {
 			EthBridge::validator_set()
+		}
+		fn xrpl_signers() -> ValidatorSet<EthBridgeId> {
+			let door_signers = XRPLBridge::door_signers();
+			ValidatorSet {
+				proof_threshold: door_signers.len().saturating_sub(1) as u32, // tolerate 1 missing witness
+				validators: door_signers,
+				id: EthBridge::notary_set_id(), // the set Id is the same as the overall Ethy set Id
+			}
 		}
 	}
 }
