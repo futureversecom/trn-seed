1c10755bd7828590dea511b7355639f083605677
Jason <jason.j.tulp@gmail.com>
2022-10-19 21:49:18 +1300

    Tidy up imports

--- pallet/nft-peg/src/lib.rs
+++ pallet/nft-peg/src/lib.rs
@@ -1,21 +1,32 @@
+/* Copyright 2021 Centrality Investments Limited
+ *
+ * Licensed under the LGPL, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * You may obtain a copy of the License at the root of this project source code,
+ * or at:
+ *     https://centrality.ai/licenses/gplv3.txt
+ *     https://centrality.ai/licenses/lgplv3.txt
+ */
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use codec::{Decode, Encode, MaxEncodedLen};
 use core::marker::PhantomData;
-
 use ethabi::{ParamType, Token};
-// Pallet for managing NFTs bridged from *x* chain
-use frame_support::{ensure, fail, traits::Get, BoundedVec, PalletId};
+use frame_support::{ensure, fail, traits::Get, weights::Weight, BoundedVec, PalletId};
 use pallet_nft::OriginChain;
 use scale_info::TypeInfo;
+use seed_pallet_common::{EthereumBridge, EthereumEventSubscriber};
 use seed_primitives::{CollectionUuid, SerialNumber};
 use sp_core::{H160, U256};
 use sp_runtime::{traits::AccountIdConversion, DispatchError, SaturatedConversion};
-
-use codec::{Decode, Encode, MaxEncodedLen};
-use frame_support::weights::Weight;
-pub use pallet::*;
-use seed_pallet_common::{EthereumBridge, EthereumEventSubscriber};
 use sp_std::{boxed::Box, vec, vec::Vec};
+
+pub use pallet::*;
 
 #[cfg(test)]
 pub mod mock;
