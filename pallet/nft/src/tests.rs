9b9ecca9314d326502efb69205a9441a1874c4d2
justinFrevert <81839854+justinFrevert@users.noreply.github.com>
2022-11-02 21:20:38 -0400

    Add fix for broken token_uris of bridged nfts (#245)
    * Add fix for broken token_uris of bridged nfts
    
    * fmt
    
    * Clean up test
    
    * fmt
    
    * fix ethereum address strings in token uri creation
    
    * fmt again?

--- pallet/nft/src/tests.rs
+++ pallet/nft/src/tests.rs
@@ -27,6 +27,7 @@ use seed_primitives::TokenId;
 use sp_core::H160;
 use sp_runtime::Permill;
 use sp_std::collections::btree_map::BTreeMap;
+use std::fmt::Write;
 
 // Create an NFT collection
 // Returns the created `collection_id`
@@ -2235,6 +2236,25 @@ fn token_uri_construction() {
 		assert_eq!(
 			Nft::token_uri((collection_id, 1)),
 			b"ipfs://bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi.json".to_vec(),
+		);
+
+		let collection_address = H160::from_low_u64_be(123);
+		let token_id = 1;
+
+		collection_id = Nft::next_collection_uuid().unwrap();
+		assert_ok!(Nft::create_collection(
+			Some(owner).into(),
+			b"test-collection".to_vec(),
+			quantity,
+			None,
+			None,
+			MetadataScheme::Ethereum(collection_address),
+			None,
+		));
+
+		assert_eq!(
+			Nft::token_uri((collection_id, token_id)),
+			b"ethereum://0x000000000000000000000000000000000000007b/1".to_vec()
 		);
 	});
 }
