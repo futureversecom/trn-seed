7e6bffb59afa3e797534b8090aa8d8212c9f4b71
Justin Frevert <justinfrevert@gmail.com>
2022-11-07 21:04:39 -0500

    fmt

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -10,7 +10,9 @@ include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 use codec::{Decode, Encode};
 use fp_rpc::TransactionStatus;
 use frame_election_provider_support::{generate_solution_type, onchain, SequentialPhragmen};
-use pallet_ethereum::{Call::transact, Transaction as EthereumTransaction, InvalidTransactionWrapper};
+use pallet_ethereum::{
+	Call::transact, InvalidTransactionWrapper, Transaction as EthereumTransaction,
+};
 use pallet_evm::{
 	Account as EVMAccount, EnsureAddressNever, EvmConfig, FeeCalculator, Runner as RunnerT,
 };
