fcf74233caa731df62a0b453681b7bdabbe02827
zees-dev <63374656+zees-dev@users.noreply.github.com>
2022-11-21 11:43:06 +1300

    Evm gas estimates fixed (#268)
    * removed redundant unreferenced vars in constants
    
    * fixed MAXIMUM_BLOCK_WEIGHT to align to frontier and cennznet
    
    * fixed GAS_PER_SECOND to align to cennznet
    
    * fc-rpc - using rpc_binary_search_estimate feature for fixing gas estimate
    
    * integration tests for gas estimation fixes - on simple contract
    
    * test added to package.json
    
    * readme markdown lint
    
    * formatting fixes - via prettier config
    
    * formatting fixes - via prettier config
    
    * changed back MAXIMUM_BLOCK_WEIGHT to 15M
    
    * removed pub modifier
    
    * reverted back GAS_PER_SECOND to 15m; address in another PR

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -147,7 +147,7 @@ const AVERAGE_ON_INITIALIZE_RATIO: Perbill = Perbill::from_percent(10);
 /// We allow `Normal` extrinsics to fill up the block up to 75%, the rest can be used
 /// by  Operational  extrinsics.
 const NORMAL_DISPATCH_RATIO: Perbill = Perbill::from_percent(75);
-/// We allow for .5 seconds of compute with a 12 second average block time.
+/// We allow for 2 seconds of compute with a 4 second average block time.
 const MAXIMUM_BLOCK_WEIGHT: Weight = WEIGHT_PER_SECOND / 2;
 
 parameter_types! {
