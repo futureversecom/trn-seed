f084b327189c22fff7803330e3a2f7faa7fce2be
zees-dev <dev.786zshan@gmail.com>
2022-11-23 12:25:48 +1300

    updated test to make it re-runnable

--- test-ts/test/TxFeePotFeeAccruel.test.ts
+++ test-ts/test/TxFeePotFeeAccruel.test.ts
@@ -3,7 +3,7 @@ import { ApiPromise, Keyring, WsProvider } from "@polkadot/api";
 import type { KeyringPair } from "@polkadot/keyring/types";
 import { hexToU8a } from "@polkadot/util";
 import { expect } from "chai";
-import { Contract, Wallet } from "ethers";
+import { Contract, utils, Wallet } from "ethers";
 import { ethers } from "hardhat";
 import web3 from "web3";
 
@@ -33,16 +33,13 @@ describe("TxFeePot fees accruel", () => {
 		// EVM variables
 		const provider = new JsonRpcProvider(`http://localhost:9933`);
 		aliceSigner = new Wallet(ALICE_PRIVATE_KEY).connect(provider);
-	});
 
-	it("TxFeePot has 0 accrued fees", async () => {
-		const currentAccruedFees = +(
+		accruedFees = +(
 			await api.query.txFeePot.eraTxFees()
 		).toString();
-		expect(currentAccruedFees).to.equal(0);
-
-		accruedFees = currentAccruedFees;
 	});
+
+	// TODO: TOTAL issuance check before and at the end
 
 	it("Contract creation transaction accrues base fee in TxFeePot", async () => {
 		const TestFactory = await ethers.getContractFactory("TestCall");
@@ -79,7 +76,7 @@ describe("TxFeePot fees accruel", () => {
 				// mint 1M tokens (18 decimals) to alice
 				FIRST_ASSET_ID,
 				alice.address,
-				"1000000000000000000000000"
+				utils.parseEther("1").toString(),
 			),
 		];
 		await new Promise<void>((resolve) => {
@@ -89,7 +86,7 @@ describe("TxFeePot fees accruel", () => {
 		});
 		console.log("Created and minted asset:", FIRST_ASSET_ID);
 
-		const feesFromExtrinsic = 340_787;
+		const feesFromExtrinsic = 335_787;
 		const currentAccruedFees = +(
 			await api.query.txFeePot.eraTxFees()
 		).toString();
@@ -105,12 +102,12 @@ describe("TxFeePot fees accruel", () => {
 		);
 		const erc20 = new Contract(erc20TokenAddress, ERC20_ABI, aliceSigner);
 		const tx = await erc20.transfer(
-			"0x0000000000000000000000000000000000000000",
+			"0x000000000000000000000000000000000000DEAD",
 			1
 		);
 		await tx.wait();
 
-		const feesFromPrecompile = 69_662;
+		const feesFromPrecompile = 69_671;
 		const currentAccruedFees = +(
 			await api.query.txFeePot.eraTxFees()
 		).toString();
