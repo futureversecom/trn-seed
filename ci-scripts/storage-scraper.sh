586bb54e63801ab5e0597a01613defba8bd9a974
Marko Petrlic <petrlic.marko@gmail.com>
2023-01-20 19:33:15 +0100

    Fixed error

--- ci-scripts/storage-scraper.sh
+++ ci-scripts/storage-scraper.sh
@@ -3,7 +3,7 @@ cargo build --locked --release
 
 # Copy binary and chain spec
 mkdir ./ci-scripts/storage-scraper/data
-cp ./target/release/seed ./ci-scripts/storage-scraper/data/
+cp ./target/release/seed ./ci-scripts/storage-scraper/data/binary
 cp ./chain-spec/* ./ci-scripts/storage-scraper/data/
 
 cd ./ci-scripts/storage-scraper
