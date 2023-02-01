6ff22f8cf66e5691f44777522a6f6298c50945c6
zees-dev <dev.786zshan@gmail.com>
2023-02-02 08:31:41 +1300

    benchmarking pallet-evm-chain-id

---
+++ pallet/evm-chain-id/src/benchmarking.rs
@@ -0,0 +1,34 @@
+// /* Copyright 2019-2021 Centrality Investments Limited
+// *
+// * Licensed under the LGPL, Version 3.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// * You may obtain a copy of the License at the root of this project source code,
+// * or at:
+// * https://centrality.ai/licenses/gplv3.txt
+// * https://centrality.ai/licenses/lgplv3.txt
+// */
+#![cfg(feature = "runtime-benchmarks")]
+
+use super::*;
+
+use crate::Pallet as EVMChainId;
+
+use frame_benchmarking::{benchmarks, impl_benchmark_test_suite};
+use frame_system::RawOrigin;
+
+benchmarks! {
+	set_chain_id {
+		assert_eq!(ChainId::<T>::get(), T::DefaultChainId::get());
+	}: _(RawOrigin::Root, 1234)
+	verify {
+		assert_eq!(EVMChainId::<T>::chain_id(), 1234);
+	}
+
+}
+
+impl_benchmark_test_suite!(EVMChainId, crate::mock::TestExt::default().build(), crate::mock::TestRuntime);
