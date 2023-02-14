d1aeb2855520edf57d7a77054ba285c6da1f4953
Ken Vu <97480229+ken-centrality@users.noreply.github.com>
2023-02-15 09:22:53 +1300

    Release 0.0.32 (#449)
    Co-authored-by: Marko Petrlic <petrlic.marko@gmail.com>

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -141,7 +141,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("root"),
 	impl_name: create_runtime_str!("root"),
 	authoring_version: 1,
-	spec_version: 27,
+	spec_version: 28,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
