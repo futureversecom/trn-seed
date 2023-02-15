4a9d7be68601eeba58725a65be6b3db593e1f488
JasonTulp <jason.j.tulp@gmail.com>
2023-02-15 13:31:13 +1300

    Add Logs

--- pallet/nft/src/impls.rs
+++ pallet/nft/src/impls.rs
@@ -241,10 +241,13 @@ impl<T: Config> Pallet<T> {
 		cursor: SerialNumber,
 		limit: u16,
 	) -> (SerialNumber, Vec<SerialNumber>) {
+		log!(trace, "🃏 Entering owned_tokens function");
+
 		let collection_info = match Self::collection_info(collection_id) {
 			Some(info) => info,
 			None => return (Default::default(), Default::default()),
 		};
+		log!(trace, "🃏 Collection Info got");
 
 		// Collect all tokens owned by address
 		let mut owned_tokens: Vec<SerialNumber> = match collection_info
@@ -256,6 +259,7 @@ impl<T: Config> Pallet<T> {
 			Some(token_ownership) => token_ownership.owned_serials.clone().into_inner(),
 			None => vec![],
 		};
+		log!(trace, "🃏 Owned Tokens got");
 
 		// Sort the vec to ensure no tokens are missed
 		owned_tokens.sort();
@@ -269,6 +273,7 @@ impl<T: Config> Pallet<T> {
 			.filter(|serial_number| serial_number >= &cursor)
 			.take(sp_std::cmp::min(limit, MAX_OWNED_TOKENS_LIMIT).into())
 			.collect();
+		log!(trace, "🃏 Response got: {:?}", response);
 
 		let new_cursor: SerialNumber = match response.last().copied() {
 			Some(highest) => {
@@ -283,6 +288,7 @@ impl<T: Config> Pallet<T> {
 			},
 			None => 0,
 		};
+		log!(trace, "🃏 New cursor got: {:?}", new_cursor);
 
 		(new_cursor, response)
 	}
