8406a368d2a76d9c3eefa622fb5f863bf3f880cb
Marko Petrlić <petrlic.marko@gmail.com>
2023-03-12 22:19:33 +0100

    Decreased evm througput from 613 to 476 (#470)
    * Decreased evm througput from 613 to 476
    
    * PR fix

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -886,8 +886,8 @@ impl pallet_evm_chain_id::Config for Runtime {
 /// Current approximation of the gas/s consumption considering
 /// EVM execution over compiled WASM (on 4.4Ghz CPU).
 /// Given the 500ms Weight, from which 75% only are used for transactions,
-/// the total EVM execution gas limit is: GAS_PER_SECOND * 0.500 * 0.75 ~= 15_000_000.
-pub const GAS_PER_SECOND: u64 = 40_000_000;
+/// the total EVM execution gas limit is: GAS_PER_SECOND * 0.500 * 0.75 ~= 11_250_000.
+pub const GAS_PER_SECOND: u64 = 30_000_000;
 
 /// Approximate ratio of the amount of Weight per Gas.
 /// u64 works for approximations because Weight is a very small unit compared to gas.
