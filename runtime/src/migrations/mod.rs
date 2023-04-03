bb8929bcd4f431eeb63846da831d032d7be91161
Marko Petrlic <petrlic.marko@gmail.com>
2023-04-03 09:54:03 +0200

    Added documentation

--- runtime/src/migrations/mod.rs
+++ runtime/src/migrations/mod.rs
@@ -71,7 +71,10 @@ impl Value {
 	/// return an Err.
 	///
 	/// Usage:
+	/// 	// To get the exact value we can use assert_eq
 	/// 	assert_eq!(Value::storage_get::<my_pallet::MyStorage::<Runtime>, _>(), Ok(expected_value));
+	///
+	/// 	// If we don't care what's the value and just wanna know that the storage is not corrupted
 	/// 	assert_ok!(Value::storage_get::<my_pallet::MyStorage::<Runtime>, _>());
 	/// 	assert_err!(Value::storage_get::<MyStorage::<Runtime>, _>());
 	#[allow(dead_code)]
@@ -85,5 +88,54 @@ impl Value {
 		}
 
 		Storage::try_get()
+	}
+
+	/// Checks if a value explicitly exist in storage.
+	///
+	/// This function is unsafe since the caller is responsible for passing the right module and
+	/// item name. For the safe/typed approach check out the `exists` function
+	///
+	/// Additional info:
+	/// 	To see if the data is corrupted or not you can call unsafe_storage_get.
+	///
+	/// Usage:
+	/// 	let module = b"MyPallet";
+	/// 	let item = b"MyStorage";
+	///
+	/// 	assert_eq!(Value::unsafe_exists(module, item), false);
+	#[allow(dead_code)]
+	pub fn unsafe_exists(module: &[u8], item: &[u8]) -> bool {
+		have_storage_value(module, item, b"")
+	}
+
+	/// This function has two roles:
+	/// 1. It returns a value explicitly from the storage
+	/// 2. It checks if the storage is corrupted or not
+	///
+	/// This function is unsafe since the caller is responsible for passing the right module and
+	/// item name. For the safe/typed approach check out the `storage_get` function
+	///
+	/// If a value is not explicitly stored in the storage it will return None.
+	/// If a value is stored in the storage but it is corrupted (of different type/size) it will
+	/// return None.
+	///
+	/// Usage:
+	/// 	assert_eq!(Value::storage_get::<my_pallet::MyStorage::<Runtime>, _>(), Ok(expected_value));
+	/// 	assert_ok!(Value::storage_get::<my_pallet::MyStorage::<Runtime>, _>());
+	/// 	assert_err!(Value::storage_get::<MyStorage::<Runtime>, _>());
+	#[allow(dead_code)]
+	pub fn unsafe_storage_get<T>(module: &[u8], item: &[u8]) -> Option<T>
+	where
+		T: Decode + Sized,
+	{
+		get_storage_value::<T>(module, item, b"")
+	}
+
+	#[allow(dead_code)]
+	pub fn unsafe_storage_put<T>(module: &[u8], item: &[u8], value: T)
+	where
+		T: Encode,
+	{
+		put_storage_value::<T>(module, item, b"", value)
 	}
 }
