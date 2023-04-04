7810638312ab868d7f6606200cdef93d2ab522ed
zees-dev <dev.786zshan@gmail.com>
2023-04-05 08:24:37 +1200

    e2e test setup and running improvements

--- e2e/common/node.ts
+++ e2e/common/node.ts
@@ -1,24 +1,13 @@
 // Adapted  from webb-tools - https://github.com/webb-tools/dkg-substrate/blob/0d86b54f57a38881ef0e555ec757b5324e5c8ca7/dkg-test-suite/tests/utils/setup.ts#L138
+import * as dotenv from "dotenv";
 import { ApiPromise, WsProvider } from "@polkadot/api";
 import child from "child_process";
 
-// a global variable to check if the node is already running or not.
-// to avoid running multiple nodes with the same authority at the same time.
-const __NODE_STATE: {
-  [authorityId: string]: {
-    process: child.ChildProcess | null;
-    isRunning: boolean;
-  };
-} = {
-  alice: { isRunning: false, process: null },
-  bob: { isRunning: false, process: null },
-  charlie: { isRunning: false, process: null },
-};
+dotenv.config();
 
 export type ConnectionType = "local" | "binary" | "docker";
 
 interface NodeOpts {
-  type: ConnectionType;
   httpPort: number;
   wsPort: number;
   dockerOpts: {
@@ -31,7 +20,6 @@ interface NodeOpts {
 }
 
 const defaultOpts: NodeOpts = {
-  type: "docker",
   httpPort: 9933,
   wsPort: 9944,
   dockerOpts: {
@@ -55,10 +43,15 @@ export interface NodeProcess {
 /**
  * Start a node given connection type
  */
-export function startNode(nodeOpts?: NodeOpts): Promise<NodeProcess> {
+export function startNode(
+  type: ConnectionType = process.env.CONNECTION_TYPE as ConnectionType ?? "docker",
+  nodeOpts?: NodeOpts,
+): Promise<NodeProcess> {
+  console.info(`Starting node with connection type: ${type}`)
+
   const nodeOptions = nodeOpts ?? defaultOpts;
 
-  if (nodeOptions.type === "local") {
+  if (type === "local") {
     // connect to an already running node
     const wsPortStr = nodeOptions.wsPort.toString() ?? "9944";
     return Promise.resolve({
@@ -72,17 +65,17 @@ export function startNode(nodeOpts?: NodeOpts): Promise<NodeProcess> {
     });
   }
 
-  if (nodeOptions.type === "docker") {
+  if (type === "docker") {
     // start a node in docker
     return startStandaloneDockerNode(nodeOptions);
   }
-  if (nodeOptions.type === "binary") {
+  if (type === "binary") {
     // TODO integrate startStandaloneNode; path param may be required
     // return startBinaryNode(httpPort, wsPort);
-    throw new Error(`Unsupported connection type: ${nodeOptions.type}`);
+    throw new Error(`Unsupported connection type: ${type}`);
   }
 
-  throw new Error(`Unknown connection type: ${nodeOptions.type}`);
+  throw new Error(`Unknown connection type: ${type}`);
 }
 
 interface DockerInspect {
@@ -185,56 +178,4 @@ async function startStandaloneDockerNode(nodeOpts: NodeOpts): Promise<NodeProces
     },
     stop,
   };
-}
-
-export function startStandaloneNode(
-  authority: "alice" | "bob" | "charlie",
-  options: { tmp: boolean; printLogs: boolean } = {
-    tmp: true,
-    printLogs: false,
-  },
-): child.ChildProcess {
-  if (__NODE_STATE[authority].isRunning) {
-    return __NODE_STATE[authority].process!;
-  }
-
-  const nodePath = "../target/release/seed";
-  const ports = {
-    alice: { ws: 9944, http: 9933, p2p: 30333 },
-    bob: { ws: 9945, http: 9934, p2p: 30334 },
-    charlie: { ws: 9946, http: 9935, p2p: 30335 },
-  };
-
-  const aliceNodeId = "12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp";
-
-  const proc = child.spawn(nodePath, [
-    "--dev",
-    options.printLogs ? "-linfo" : "-lerror",
-    `--ws-port=${ports[authority].ws}`,
-    `--rpc-port=${ports[authority].http}`,
-    `--port=${ports[authority].p2p}`,
-    ...(authority == "alice"
-      ? ["--node-key", "0000000000000000000000000000000000000000000000000000000000000001"]
-      : ["--bootnodes", `/ip4/127.0.0.1/tcp/${ports["alice"].p2p}/p2p/${aliceNodeId}`]),
-    // only print logs from the alice node
-    ...(authority === "alice" && options.printLogs ? ["--rpc-cors", "all", "--ws-external"] : []),
-  ]);
-  __NODE_STATE[authority].isRunning = true;
-  __NODE_STATE[authority].process = proc;
-
-  proc.stdout.on("data", (data) => {
-    process.stdout.write(data);
-  });
-
-  proc.stderr.on("data", (data) => {
-    process.stdout.write(data);
-  });
-
-  proc.on("close", (code) => {
-    __NODE_STATE[authority].isRunning = false;
-    __NODE_STATE[authority].process = null;
-    console.log(`${authority} node exited with code ${code}`);
-  });
-
-  return proc;
 }
