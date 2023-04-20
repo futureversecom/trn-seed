3a2dce4c2b58f59dbdccafe3e403021d20d0b900
Marko Petrlić <petrlic.marko@gmail.com>
2023-04-20 11:13:31 +0200

    Chore/213 remove t out of collectioninformation (#525)
    * Made changes so that it can compile
    
    * Fixed tests
    
    * Made the error handling more elegant
    
    * QoL change
    
    * Fixed warning
    
    * PR change

--- pallet/nft/src/traits.rs
+++ pallet/nft/src/traits.rs
@@ -9,14 +9,16 @@
 // limitations under the License.
 // You may obtain a copy of the License at the root of this project source code
 
+use frame_support::traits::Get;
 use seed_primitives::{CollectionUuid, MetadataScheme, TokenCount, TokenId};
 use sp_runtime::{DispatchError, DispatchResult};
+use sp_std::fmt::Debug;
 
-use crate::{CollectionInformation, CollectionNameType, Config, OriginChain, RoyaltiesSchedule};
+use crate::{CollectionInformation, CollectionNameType, OriginChain, RoyaltiesSchedule};
 
 pub trait NFTExt {
-	type AccountId;
-	type T: Config;
+	type AccountId: Debug + PartialEq + Clone;
+	type MaxTokensPerCollection: Get<u32>;
 
 	fn do_mint(
 		origin: Self::AccountId,
@@ -40,7 +42,7 @@ pub trait NFTExt {
 
 	fn get_collection_info(
 		collection_id: CollectionUuid,
-	) -> Result<CollectionInformation<Self::T>, DispatchError>;
+	) -> Result<CollectionInformation<Self::AccountId, Self::MaxTokensPerCollection>, DispatchError>;
 
 	fn enable_xls20_compatibility(
 		who: Self::AccountId,
