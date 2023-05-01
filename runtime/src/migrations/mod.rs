3b18ad21fb009cab7bcc054591e54ed2b742b7d8
justinFrevert <81839854+justinFrevert@users.noreply.github.com>
2023-04-30 21:47:53 -0400

    release/2.33.0 (#580)
    * release/2.33.0
    
    * forgot to update Cargo.lock
    
    * Removed old migration
    
    * Remove unused variable/comment
    
    ---------
    
    Co-authored-by: Marko Petrlic <petrlic.marko@gmail.com>

--- runtime/src/migrations/mod.rs
+++ runtime/src/migrations/mod.rs
@@ -9,7 +9,6 @@
 // limitations under the License.
 // You may obtain a copy of the License at the root of this project source code
 
-mod fee_control;
 mod nft;
 mod xrpl_bridge;
 
@@ -30,7 +29,6 @@ pub struct AllMigrations;
 impl OnRuntimeUpgrade for AllMigrations {
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<(), &'static str> {
-		fee_control::Upgrade::pre_upgrade()?;
 		nft::Upgrade::pre_upgrade()?;
 		xrpl_bridge::Upgrade::pre_upgrade()?;
 
@@ -39,7 +37,6 @@ impl OnRuntimeUpgrade for AllMigrations {
 
 	fn on_runtime_upgrade() -> Weight {
 		let mut weight = Weight::from(0u32);
-		weight += fee_control::Upgrade::on_runtime_upgrade();
 		weight += nft::Upgrade::on_runtime_upgrade();
 		weight += xrpl_bridge::Upgrade::on_runtime_upgrade();
 
@@ -48,7 +45,6 @@ impl OnRuntimeUpgrade for AllMigrations {
 
 	#[cfg(feature = "try-runtime")]
 	fn post_upgrade() -> Result<(), &'static str> {
-		fee_control::Upgrade::post_upgrade()?;
 		nft::Upgrade::post_upgrade()?;
 		xrpl_bridge::Upgrade::post_upgrade()?;
 
