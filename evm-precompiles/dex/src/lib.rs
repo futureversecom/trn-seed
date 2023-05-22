7d7d17393ba3e9229ab97d9ef0a9aeac3759c9da
Han Zhao <han.zhao@futureverse.com>
2023-05-23 07:57:13 +0800

    Implement dex precompiles with add liquidity functions

---
+++ evm-precompiles/dex/src/lib.rs
@@ -0,0 +1,215 @@
+// Copyright 2022-2023 Futureverse Corporation Limited
+//
+// Licensed under the LGPL, Version 3.0 (the "License");
+// you may not use this file except in compliance with the License.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// You may obtain a copy of the License at the root of this project source code
+
+#![cfg_attr(not(feature = "std"), no_std)]
+extern crate alloc;
+
+use fp_evm::{PrecompileHandle, PrecompileOutput, PrecompileResult};
+use frame_support::{
+	dispatch::{Dispatchable, GetDispatchInfo, PostDispatchInfo},
+	ensure,
+};
+use pallet_evm::{GasWeightMapping, Precompile};
+use precompile_utils::{constants::ERC721_PRECOMPILE_ADDRESS_PREFIX, prelude::*};
+use seed_primitives::{AssetId, Balance, BlockNumber, CollectionUuid, MetadataScheme, TokenCount};
+use sp_core::{H160, U256};
+use sp_runtime::{traits::SaturatedConversion, Permill};
+use sp_std::{marker::PhantomData, vec::Vec};
+
+/// Solidity selector of the AddLiquidity log, which is the Keccak of the Log signature.
+pub const SELECTOR_LOG_ADD_LIQUIDITY: [u8; 32] = keccak256!("AddLiquidity(address,address)");
+
+#[generate_function_selector]
+#[derive(Debug, PartialEq)]
+pub enum Action {
+	AddLiquidity = "addLiquidity(address,address,uint,uint,uint,uint,address,uint)",
+	AddLiquidityETH = "addLiquidityETH(address,uint,uint,uint,address,uint)",
+	RemoveLiquidity = "removeLiquidity(address,address,uint,uint,uint,address,uint)",
+	RemoveLiquidityETH = "removeLiquidityETH(address,uint,uint,uint,address,uint)",
+	RemoveLiquidityWithPermit = "removeLiquidityWithPermit(address,address,uint,uint,uint,address,uint,bool,uint8,bytes32,bytes32)",
+	RemoveLiquidityETHWithPermit = "removeLiquidityETHWithPermit(address,uint,uint,uint,address,uint,bool,uint8,bytes32,bytes32)",
+	SwapExactTokensForTokens = "swapExactTokensForTokens(uint,uint,address[],address,uint)",
+	SwapTokensForExactTokens = "swapTokensForExactTokens(uint,uint,address[],address,uint)",
+	SwapExactETHForTokens = "swapExactETHForTokens(uint,address[],address,uint)",
+	SwapTokensForExactETH = "swapTokensForExactETH(uint,uint,address[],address,uint)",
+	SwapExactTokensForETH = "swapExactTokensForETH(uint,uint,address[],address,uint)",
+	SwapETHForExactTokens = "swapETHForExactTokens(uint,address[],address,uint)",
+	Quote = "quote(uint,uint,uint)",
+	GetAmountOut = "getAmountOut(uint,uint,uint)",
+	GetAmountIn = "getAmountIn(uint,uint,uint)",
+	GetAmountsOut = "getAmountsOut(uint,address[])",
+	GetAmountsIn = "getAmountsIn(uint,address[])",
+}
+
+/// Provides access to the Dex pallet
+pub struct DexPrecompile<Runtime>(PhantomData<Runtime>);
+
+impl<T> Default for DexPrecompile<T> {
+	fn default() -> Self {
+		Self(PhantomData)
+	}
+}
+
+impl<Runtime> Precompile for DexPrecompile<Runtime>
+where
+	Runtime::AccountId: From<H160> + Into<H160>,
+	Runtime::BlockNumber: Into<U256>,
+	Runtime: frame_system::Config + pallet_dex::Config + pallet_evm::Config,
+	Runtime: ErcIdConversion<CollectionUuid, EvmId = Address>,
+	Runtime::Call: Dispatchable<PostInfo = PostDispatchInfo> + GetDispatchInfo,
+	Runtime::Call: From<pallet_dex::Call<Runtime>>,
+	<Runtime::Call as Dispatchable>::Origin: From<Option<Runtime::AccountId>>,
+{
+	fn execute(handle: &mut impl PrecompileHandle) -> PrecompileResult {
+		let result = {
+			let selector = match handle.read_selector() {
+				Ok(selector) => selector,
+				Err(e) => return Err(e.into()),
+			};
+
+			if let Err(err) = handle.check_function_modifier(FunctionModifier::NonPayable) {
+				return Err(err.into())
+			}
+
+			match selector {
+				Action::AddLiquidity => Self::add_liquidity(handle),
+				Action::AddLiquidityETH => Self::add_liquidity_eth(handle),
+				_ => return Err(revert("DEX: Function not implemented").into()),
+			}
+		};
+		return result
+	}
+}
+
+impl<Runtime> DexPrecompile<Runtime> {
+	pub fn new() -> Self {
+		Self(PhantomData)
+	}
+}
+
+impl<Runtime> DexPrecompile<Runtime>
+where
+	Runtime::AccountId: From<H160> + Into<H160>,
+	Runtime::BlockNumber: Into<U256>,
+	Runtime: frame_system::Config + pallet_dex::Config + pallet_evm::Config,
+	Runtime: ErcIdConversion<CollectionUuid, EvmId = Address>,
+	Runtime::Call: Dispatchable<PostInfo = PostDispatchInfo> + GetDispatchInfo,
+	Runtime::Call: From<pallet_dex::Call<Runtime>>,
+	<Runtime::Call as Dispatchable>::Origin: From<Option<Runtime::AccountId>>,
+{
+	fn add_liquidity(handle: &mut impl PrecompileHandle) -> EvmResult<PrecompileOutput> {
+		handle.record_log_costs_manual(1, 32)?;
+
+		// Parse input.
+		read_args!(
+			handle,
+			{
+				asset_id_a: AssetId,
+				asset_id_b: AssetId,
+				amount_a_desired: Balance,
+				amount_b_desired: Balance,
+				amount_a_min: Balance,
+				amount_b_min: Balance,
+				to: AssetId,
+				deadline: U256
+			}
+		);
+
+		let caller = handle.context().caller;
+		let current_block_number = frame_system::Pallet::<Runtime>::block_number();
+
+		// Make sure the deadline meets
+		ensure!(
+			deadline >= current_block_number.into(),
+			revert("DEX: The deadline has been missed.")
+		);
+
+		// Dispatch call (if enough gas).
+		RuntimeHelper::<Runtime>::try_dispatch(
+			handle,
+			Some(caller.into()).into(),
+			pallet_dex::Call::<Runtime>::add_liquidity {
+				asset_id_a,
+				asset_id_b,
+				amount_a_desired,
+				amount_b_desired,
+				amount_a_min,
+				amount_b_min,
+				min_share_increment: 0,
+			},
+		)?;
+
+		log2(
+			handle.code_address(),
+			SELECTOR_LOG_ADD_LIQUIDITY,
+			caller,
+			EvmDataWriter::new().write(AssetId::from(asset_id_a)).build(),
+		)
+		.record(handle)?;
+
+		// Build output.
+		Ok(succeed([]))
+	}
+
+	fn add_liquidity_eth(handle: &mut impl PrecompileHandle) -> EvmResult<PrecompileOutput> {
+		handle.record_log_costs_manual(7, 32)?;
+
+		// Parse input.
+		read_args!(
+			handle,
+			{
+				asset_id_a: AssetId,
+				asset_id_b: AssetId,
+				amount_a_desired: Balance,
+				amount_b_desired: Balance,
+				amount_a_min: Balance,
+				amount_b_min: Balance,
+				to: AssetId,
+				deadline: U256
+			}
+		);
+
+		let caller = handle.context().caller;
+		let current_block_number = frame_system::Pallet::<Runtime>::block_number();
+
+		// Make sure the deadline meets
+		ensure!(
+			deadline >= current_block_number.into(),
+			revert("DEX: The deadline has been missed.")
+		);
+
+		// Dispatch call (if enough gas).
+		RuntimeHelper::<Runtime>::try_dispatch(
+			handle,
+			Some(caller.into()).into(),
+			pallet_dex::Call::<Runtime>::add_liquidity {
+				asset_id_a,
+				asset_id_b,
+				amount_a_desired,
+				amount_b_desired,
+				amount_a_min,
+				amount_b_min,
+				min_share_increment: 0,
+			},
+		)?;
+
+		log2(
+			handle.code_address(),
+			SELECTOR_LOG_ADD_LIQUIDITY,
+			caller,
+			EvmDataWriter::new().write(AssetId::from(asset_id_a)).build(),
+		)
+		.record(handle)?;
+
+		// Build output.
+		Ok(succeed([]))
+	}
+}
