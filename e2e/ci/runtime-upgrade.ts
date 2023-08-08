e98856501f3f7e4c06ddf3fbb94b034f64bda65e
Han Zhao <han.zhao@futureverse.com>
2023-08-08 16:37:54 +0200

    Add runtime upgrade script

---
+++ e2e/ci/runtime-upgrade.ts
@@ -0,0 +1,59 @@
+import { ApiPromise, Keyring, WsProvider } from "@polkadot/api";
+import { KeyringPair } from "@polkadot/keyring/types";
+import { hexToU8a } from "@polkadot/util";
+
+import {
+  ALITH_PRIVATE_KEY,
+  BOB_PRIVATE_KEY,
+  GAS_TOKEN_ID,
+  NodeProcess,
+  finalizeTx,
+  getNextAssetId,
+  rpcs,
+  startNode,
+  typedefs,
+} from "../common";
+
+// Import the API & Provider and some utility functions
+
+const fs = require("fs");
+
+async function main() {
+  // Initialise the provider to connect to the dev node
+  const provider = new WsProvider("wss://forkednet.au.cicd.rootnet.app/ws");
+
+  // Create the API and wait until ready (optional provider passed through)
+  const api = await ApiPromise.create({ provider, types: typedefs, rpc: rpcs });
+
+  const keyring = new Keyring({ type: "ethereum" });
+  const alith = keyring.addFromSeed(hexToU8a(ALITH_PRIVATE_KEY));
+
+  // Retrieve the runtime to upgrade
+  const code = fs.readFileSync("../output/seed_runtime.compact.compressed.wasm").toString("hex");
+  const proposal = api.tx.system.setCode(`0x${code}`);
+
+  console.log(`Upgrading from ${alith.address}, ${code.length / 2} bytes`);
+
+  // Perform the actual chain upgrade via the sudo module
+  api.tx.sudo.sudoUncheckedWeight(proposal, 0).signAndSend(alith, ({ events = [], status }) => {
+    console.log("Proposal status:", status.type);
+
+    if (status.isInBlock) {
+      console.error("You have just upgraded your chain");
+
+      console.log("Included at block hash", status.asInBlock.toHex());
+      console.log("Events:");
+
+      console.log(JSON.stringify(events, null, 2));
+    } else if (status.isFinalized) {
+      console.log("Finalized block hash", status.asFinalized.toHex());
+
+      process.exit(0);
+    }
+  });
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(-1);
+});
