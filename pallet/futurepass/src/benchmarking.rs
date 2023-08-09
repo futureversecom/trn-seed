ed85fe23964da88ba85fcdceefdcedf3a6196b0c
surangap <19677661+surangap@users.noreply.github.com>
2023-08-09 12:20:28 +1200

    Fix benchmark for register_delegate_with_signature

--- pallet/futurepass/src/benchmarking.rs
+++ pallet/futurepass/src/benchmarking.rs
@@ -41,6 +41,26 @@ where
 	assert_ok!(T::MultiCurrency::mint_into(root_asset_id.into(), &account, 1_000_000u32.into()));
 }
 
+pub fn add_delegates<T: Config>(
+	n: u32,
+	futurepass: T::AccountId,
+	maybe_who: Option<T::AccountId>,
+) -> Result<(), &'static str>
+where
+	<T as frame_system::Config>::AccountId: From<sp_core::H160>,
+{
+	let caller = maybe_who.unwrap_or_else(whitelisted_caller);
+	for i in 0..n {
+		T::Proxy::add_delegate(
+			&caller,
+			&futurepass,
+			&account::<T::AccountId>("trarget", i, 0),
+			&T::ProxyType::default().into(),
+		)?;
+	}
+	Ok(())
+}
+
 benchmarks! {
 	where_clause { where <T as frame_system::Config>::AccountId: From<sp_core::H160> }
 	create {
@@ -54,11 +74,13 @@ benchmarks! {
 	}
 
 	register_delegate_with_signature {
-		let owner: T::AccountId = account("account", 0, 0);
+		let p in 1 .. (32 - 1);
 
+		let owner: T::AccountId = account("account", 0, 0);
 		fund::<T>(&owner);
 		assert_ok!(Futurepass::<T>::create(RawOrigin::Signed(owner.clone()).into(), owner.clone()));
 		let futurepass: T::AccountId = Holders::<T>::get(&owner).unwrap();
+		add_delegates::<T>(p-1, futurepass.clone(), Some(owner.clone()))?;
 		let delegate: T::AccountId = H160::from_slice(&hex!("420aC537F1a4f78d4Dfb3A71e902be0E3d480AFB")).into();
 		let proxy_type = T::ProxyType::default();
 		let deadline: u32 = 200;
