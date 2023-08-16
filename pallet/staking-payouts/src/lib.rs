9a35bc288f18d8732f9286bca56e49ae57c73924
Justin Frevert <justinfrevert@gmail.com>
2023-08-16 12:44:45 -0400

    Commit partially translated state(broken)

--- pallet/staking-payouts/src/lib.rs
+++ pallet/staking-payouts/src/lib.rs
@@ -18,15 +18,11 @@
 pub use pallet::*;
 
 use frame_election_provider_support::SortedListProvider;
-use frame_support::{
-	pallet_prelude::*,
-	sp_runtime::{traits::One, SaturatedConversion},
-	storage::{generator::StorageMap, unhashed},
-	PalletId,
-};
+use frame_support::{pallet_prelude::*, traits::Currency};
 use frame_system::pallet_prelude::*;
 use seed_primitives::AccountId;
-use sp_core::H160;
+use sp_runtime::Perbill;
+use sp_staking::EraIndex;
 use sp_std::prelude::*;
 
 #[cfg(feature = "runtime-benchmarks")]
@@ -41,10 +37,9 @@ pub use weights::WeightInfo;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::storage::hashed;
-	use pallet_staking::UseNominatorsAndValidatorsMap;
-
 	use super::*;
+	use frame_support::storage::hashed;
+	use pallet_staking::{UseNominatorsAndValidatorsMap, WeightInfo};
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub (super) trait Store)]
@@ -52,10 +47,27 @@ pub mod pallet {
 	#[pallet::without_storage_info]
 	pub struct Pallet<T>(_);
 
+	// When thinking about the currency type, consider that this is a custom Currency implementation
+	// for multiple assets
+	type PositiveImbalanceOf<T> = <<T as Config>::Currency as Currency<
+		<T as frame_system::Config>::AccountId,
+	>>::PositiveImbalance;
+	type NegativeImbalanceOf<T> = <<T as Config>::Currency as Currency<
+		<T as frame_system::Config>::AccountId,
+	>>::NegativeImbalance;
+
 	#[pallet::config]
 	pub trait Config: frame_system::Config<AccountId = AccountId> + pallet_staking::Config {
 		/// The system event type
 		type Event: From<Event> + IsType<<Self as frame_system::Config>::Event>;
+
+		// type Currency: LockableCurrency<
+		// 	Self::AccountId,
+		// 	Moment = Self::BlockNumber,
+		// 	Balance = Self::CurrencyBalance,
+		// >;
+
+		type Currency: Currency<Self::AccountId>;
 
 		/// Interface to access weight values
 		type WeightInfo: WeightInfo;
@@ -77,7 +89,11 @@ pub mod pallet {
 	}
 
 	#[pallet::error]
-	pub enum Error<T> {}
+	pub enum Error<T> {
+		InvalidEraToReward,
+		NotController,
+		NotStash,
+	}
 
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
@@ -109,16 +125,19 @@ pub mod pallet {
 				CurrentValidatorIter::<T>::set(validator);
 
 				if let Some(validator) = validator {
-					// Try perform queries
-					pallet_staking::ErasStakers::<T>::get(previous_era, validator);
-					pallet_staking::ErasStakersClipped::<T>::get(previous_era, validator);
-					pallet_staking::ErasValidatorReward::<T>::get(previous_era);
-					pallet_staking::ErasRewardPoints::<T>::get(previous_era);
-					pallet_staking::ErasTotalStake::<T>::get(previous_era);
-					pallet_staking::Pallet::<T>::history_depth();
-					pallet_staking::CurrentEra::<T>::get();
-					pallet_staking::Bonded::<T>::get(validator);
-					pallet_staking::Ledger::<T>::get(validator);
+					let payout_period = PayoutPeriod::<T>::get();
+
+					Self::do_payout_stakers(validator, payout_period, previous_era);
+					// // Try perform queries
+					// pallet_staking::ErasStakers::<T>::get(previous_era, validator);
+					// pallet_staking::ErasStakersClipped::<T>::get(previous_era, validator);
+					// pallet_staking::ErasValidatorReward::<T>::get(previous_era);
+					// pallet_staking::ErasRewardPoints::<T>::get(previous_era);
+					// pallet_staking::ErasTotalStake::<T>::get(previous_era);
+					// pallet_staking::Pallet::<T>::history_depth();
+					// pallet_staking::CurrentEra::<T>::get();
+					// pallet_staking::Bonded::<T>::get(validator);
+					// pallet_staking::Ledger::<T>::get(validator);
 				}
 			}
 
@@ -131,6 +150,129 @@ pub mod pallet {
 		#[pallet::weight(10000000)]
 		pub fn payout_stakers(origin: OriginFor<T>, payout_period_id: u128) -> DispatchResult {
 			Ok(())
+		}
+	}
+
+	impl<T: Config> Pallet<T> {
+		pub(super) fn do_payout_stakers(
+			validator_stash: T::AccountId,
+			// era: EraIndex,
+			payout_period: u128,
+			previous_era: EraIndex,
+		) -> DispatchResultWithPostInfo {
+			let history_depth = pallet_staking::Pallet::<T>::history_depth();
+
+			// Note: if era has no reward to be claimed, era may be future. better not to update
+			// `ledger.claimed_rewards` in this case.
+			let era_payout = pallet_staking::ErasValidatorReward::<T>::get(previous_era)
+				// TODO: :Determine weight
+				// .ok_or_else(|| Error::<T>::InvalidEraToReward.with_weight(100000))?;
+				.ok_or_else(|| Error::<T>::InvalidEraToReward)?;
+
+			let controller =
+				pallet_staking::Bonded::<T>::get(&validator_stash).ok_or_else(|| {
+					// Error::<T>::NotStash.
+					// with_weight(T::WeightInfo::payout_stakers_alive_staked(0))
+					Error::<T>::NotStash
+				})?;
+			let mut ledger =
+				pallet_staking::Ledger::<T>::get(&controller).ok_or(Error::<T>::NotController)?;
+
+			// Check this one
+			ledger
+				.claimed_rewards
+				.retain(|&x| x >= current_era.saturating_sub(history_depth));
+
+			match ledger.claimed_rewards.binary_search(&era) {
+				Ok(_) =>
+					return Err(Error::<T>::AlreadyClaimed
+						.with_weight(T::WeightInfo::payout_stakers_alive_staked(0))),
+				Err(pos) => ledger.claimed_rewards.insert(pos, era),
+			}
+
+			let exposure = pallet_staking::ErasStakersClipped::<T>::get(&era, &ledger.stash);
+
+			// Input data seems good, no errors allowed after this point
+
+			// Check this one
+			pallet_staking::Ledger::<T>::insert(&controller, &ledger);
+
+			// Get Era reward points. It has TOTAL and INDIVIDUAL
+			// Find the fraction of the era reward that belongs to the validator
+			// Take that fraction of the eras rewards to split to nominator and validator
+			//
+			// Then look at the validator, figure out the proportion of their reward
+			// which goes to them and each of their nominators.
+
+			let era_reward_points = pallet_staking::ErasRewardPoints::get(&era);
+			let total_reward_points = era_reward_points.total;
+
+			let validator_reward_points = era_reward_points
+				.individual
+				.get(&ledger.stash)
+				.copied()
+				.unwrap_or_else(Zero::zero);
+
+			// Nothing to do if they have no reward points.
+			if validator_reward_points.is_zero() {
+				return Ok(Some(T::WeightInfo::payout_stakers_alive_staked(0)).into())
+			}
+
+			// This is the fraction of the total reward that the validator and the
+			// nominators will get.
+			let validator_total_reward_part =
+				Perbill::from_rational(validator_reward_points, total_reward_points);
+
+			// This is how much validator + nominators are entitled to.
+			let validator_total_payout = validator_total_reward_part * era_payout;
+
+			let validator_prefs =
+				pallet_staking::Pallet::<T>::eras_validator_prefs(&previous_era, &validator_stash);
+			// Validator first gets a cut off the top.
+			let validator_commission = validator_prefs.commission;
+			let validator_commission_payout = validator_commission * validator_total_payout;
+
+			let validator_leftover_payout = validator_total_payout - validator_commission_payout;
+			// Now let's calculate how this is split to the validator.
+			let validator_exposure_part = Perbill::from_rational(exposure.own, exposure.total);
+			let validator_staking_payout = validator_exposure_part * validator_leftover_payout;
+
+			let mut total_imbalance = PositiveImbalanceOf::<T>::zero();
+			// We can now make total validator payout:
+			if let Some(imbalance) = Self::make_payout(
+				&ledger.stash,
+				validator_staking_payout + validator_commission_payout,
+			) {
+				Self::deposit_event(Event::<T>::Rewarded(ledger.stash, imbalance.peek()));
+				total_imbalance.subsume(imbalance);
+			}
+
+			// Track the number of payout ops to nominators. Note:
+			// `WeightInfo::payout_stakers_alive_staked` always assumes at least a validator is paid
+			// out, so we do not need to count their payout op.
+			let mut nominator_payout_count: u32 = 0;
+
+			// Lets now calculate how this is split to the nominators.
+			// Reward only the clipped exposures. Note this is not necessarily sorted.
+			for nominator in exposure.others.iter() {
+				let nominator_exposure_part =
+					Perbill::from_rational(nominator.value, exposure.total);
+
+				let nominator_reward: BalanceOf<T> =
+					nominator_exposure_part * validator_leftover_payout;
+				// We can now make nominator payout:
+				if let Some(imbalance) = Self::make_payout(&nominator.who, nominator_reward) {
+					// Note: this logic does not count payouts for `RewardDestination::None`.
+					nominator_payout_count += 1;
+					let e = Event::<T>::Rewarded(nominator.who.clone(), imbalance.peek());
+					Self::deposit_event(e);
+					total_imbalance.subsume(imbalance);
+				}
+			}
+
+			T::Reward::on_unbalanced(total_imbalance);
+			debug_assert!(nominator_payout_count <= T::MaxNominatorRewardedPerValidator::get());
+			Ok(Some(T::WeightInfo::payout_stakers_alive_staked(nominator_payout_count)).into())
 		}
 	}
 }
