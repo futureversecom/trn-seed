8d37743f8e3ee1be23b3e2aef1699563b418613b
JasonTulp <jason.j.tulp@gmail.com>
2023-08-17 14:44:41 +1200

    Remove unused mod

--- runtime/src/migrations/mod.rs
+++ runtime/src/migrations/mod.rs
@@ -14,7 +14,6 @@
 // You may obtain a copy of the License at the root of this project source code
 
 mod nft;
-mod proxy;
 
 use codec::{Decode, Encode, FullCodec, FullEncode};
 use frame_support::{
