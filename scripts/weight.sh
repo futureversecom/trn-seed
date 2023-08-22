f5714d80b48f2c05b726527b8a3925f048d3fc0f
KarishmaBothara <bothara.karishma@gmail.com>
2023-08-22 14:43:29 +0530

    Update script to include pallet

--- scripts/weight.sh
+++ scripts/weight.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-echo "function_name,weight" > weight.csv; \
-data='';
-find ./runtime/src/weights/* -type f -exec pcregrep -M  "Weight {\n(.* as u64)" {} \;  | while read line; do
-
+echo "file_name,function_name,weight" > weight.csv; \
+find ./runtime/src/weights/* -type f -exec pcregrep -H -M  "Weight {\n(.* as u64)" {} \;  | while read line; do
+file_name=$(echo $line | awk -v FS="(runtime/src/weights/|.rs:)" '{print $2}')
 function_name=$(echo $line | grep -o 'fn .*(')
 function_weight=$(echo $line | grep -Eo '([[:digit:]]+_[[:digit:]]+_[[:digit:]]+|[0])')
+
+if [[ $file_name ]]
+then
+data="${file_name}"
+fi
 if [[ $function_name ]]
 then
-data="${function_name}"
+data="${data},${function_name}"
 fi
 
 if [[ $function_weight ]]
