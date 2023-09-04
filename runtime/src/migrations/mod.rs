344d61758c0b6b0d9091057524c991838a011461
zees-dev <dev.786zshan@gmail.com>
2023-09-04 16:52:55 +1200

    removed proxy migration; introduced futurepass migration

--- runtime/src/migrations/mod.rs
+++ runtime/src/migrations/mod.rs
@@ -13,6 +13,9 @@
 // limitations under the License.
 // You may obtain a copy of the License at the root of this project source code
 
+mod futurepass;
+mod nft;
+
 use codec::{Decode, Encode, FullCodec, FullEncode};
 use frame_support::{
 	migration::{
@@ -31,16 +34,23 @@ use sp_std::vec::Vec;
 pub struct AllMigrations;
 impl OnRuntimeUpgrade for AllMigrations {
 	#[cfg(feature = "try-runtime")]
-	fn pre_upgrade() -> Result<Vec<u8>, DispatchError> {
-		Ok(Vec::new())
+	fn pre_upgrade() -> Result<Vec<u8>, &'static str> {
+		let data = nft::Upgrade::pre_upgrade()?;
+		futurepass::Upgrade::pre_upgrade()?;
+		Ok(data)
 	}
 
 	fn on_runtime_upgrade() -> Weight {
-		Weight::zero()
+		let mut weight = Weight::from_ref_time(0_u64);
+		weight += nft::Upgrade::on_runtime_upgrade();
+		weight += futurepass::Upgrade::on_runtime_upgrade();
+		weight
 	}
 
 	#[cfg(feature = "try-runtime")]
-	fn post_upgrade(_state: Vec<u8>) -> Result<(), DispatchError> {
+	fn post_upgrade(state: Vec<u8>) -> Result<(), &'static str> {
+		nft::Upgrade::post_upgrade(state)?;
+		futurepass::Upgrade::post_upgrade()?;
 		Ok(())
 	}
 }
