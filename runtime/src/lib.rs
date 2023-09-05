f2b1a9462016e20d6f534b8c36e393f8e9c74776
surangap <19677661+surangap@users.noreply.github.com>
2023-09-05 17:52:53 +1200

    Bump transaction_version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -149,7 +149,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_version: 41,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 3,
+	transaction_version: 4,
 	state_version: 0,
 };
 
