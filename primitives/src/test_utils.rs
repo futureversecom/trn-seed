76132b707cd551d43e21d06c58259b42e0f15b59
JasonTulp <jason.j.tulp@gmail.com>
2023-11-24 11:35:35 +1300

    Add with_asset to TestExt

--- primitives/src/test_utils.rs
+++ primitives/src/test_utils.rs
@@ -3,6 +3,23 @@ use crate::*;
 use frame_support::{sp_io, traits::GenesisBuild};
 use sp_core::H160;
 
+#[derive(Default)]
+struct AssetsFixture<T: frame_system::Config + pallet_assets::Config> {
+	pub id: <T as pallet_assets::Config>::AssetId,
+	pub symbol: Vec<u8>,
+	pub endowments: Vec<(T::AccountId, <T as pallet_assets::Config>::Balance)>,
+}
+
+impl<T: frame_system::Config + pallet_assets::Config> AssetsFixture<T> {
+	fn new(
+		id: <T as pallet_assets::Config>::AssetId,
+		symbol: &[u8],
+		endowments: &[(T::AccountId, <T as pallet_assets::Config>::Balance)],
+	) -> Self {
+		Self { id, symbol: symbol.to_vec(), endowments: endowments.to_vec() }
+	}
+}
+
 /// Generec TestExternalities builder to be used across all pallets
 pub struct TestExt<T: frame_system::Config + pallet_balances::Config + pallet_assets::Config> {
 	balances: Vec<(T::AccountId, <T as pallet_balances::Config>::Balance)>,
@@ -11,6 +28,7 @@ pub struct TestExt<T: frame_system::Config + pallet_balances::Config + pallet_as
 		T::AccountId,
 		<T as pallet_assets::Config>::Balance,
 	)>,
+	assets: Vec<AssetsFixture<T>>,
 	block_number: BlockNumber,
 }
 
@@ -21,7 +39,7 @@ where
 {
 	/// Create new TestExt with default values
 	fn default() -> Self {
-		Self { balances: vec![], xrp_balances: vec![], block_number: 1 }
+		Self { balances: vec![], xrp_balances: vec![], assets: vec![], block_number: 1 }
 	}
 }
 
@@ -41,6 +59,18 @@ where
 		balances: &[(T::AccountId, <T as pallet_balances::Config>::Balance)],
 	) -> Self {
 		self.balances = balances.to_vec();
+		self
+	}
+
+	/// Configure an asset with id, name and some endowments
+	/// total supply = sum(endowments)
+	pub fn with_asset(
+		mut self,
+		id: <T as pallet_assets::Config>::AssetId,
+		name: &str,
+		endowments: &[(T::AccountId, <T as pallet_assets::Config>::Balance)],
+	) -> Self {
+		self.assets.push(AssetsFixture::new(id, name.as_bytes(), endowments));
 		self
 	}
 
@@ -66,14 +96,31 @@ where
 	/// Build the Text Externalities for general use across all pallets
 	pub fn build(self) -> sp_io::TestExternalities {
 		let mut ext = frame_system::GenesisConfig::default().build_storage::<T>().unwrap();
+		let mut assets = Vec::default();
+		let mut metadata = Vec::default();
+		let mut accounts = Vec::default();
+		let default_owner = T::AccountId::from(H160::from_low_u64_be(100));
 
-		// add assets to Genesis Config
+		// add assets
+		if !self.assets.is_empty() {
+			for AssetsFixture { id, symbol, endowments } in self.assets {
+				assets.push((id, default_owner.clone(), true, 1.into()));
+				metadata.push((id, symbol.clone(), symbol, 6));
+				for (payee, balance) in endowments {
+					accounts.push((id, payee, balance));
+				}
+			}
+		}
+
+		// add xrp balances
 		if !self.xrp_balances.is_empty() {
-			let assets =
-				vec![(2.into(), T::AccountId::from(H160::from_low_u64_be(100)), true, 1.into())];
-			let metadata = vec![(2.into(), b"XRP".to_vec(), b"XRP".to_vec(), 6_u8)];
-			let accounts = self.xrp_balances;
+			assets.push((2.into(), default_owner, true, 1.into()));
+			metadata.push((2.into(), b"XRP".to_vec(), b"XRP".to_vec(), 6_u8));
+			accounts.extend(self.xrp_balances);
+		}
 
+		// Configure pallet_assets Genesis Config with assets
+		if !assets.is_empty() {
 			pallet_assets::GenesisConfig::<T> { assets, metadata, accounts }
 				.assimilate_storage(&mut ext)
 				.unwrap();
