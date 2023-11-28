21d593afc68d667944badaa1905811cf5e426899
JasonT <50426640+JasonTulp@users.noreply.github.com>
2023-11-29 12:53:53 +1300

    Fix XRPL tests (#745)
    * Fix tests
    
    * Replace create_account with helper function
    
    * Cleanup mock
    
    * Update tests

--- pallet/xrpl-bridge/src/mock.rs
+++ pallet/xrpl-bridge/src/mock.rs
@@ -13,11 +13,10 @@
 // limitations under the License.
 // You may obtain a copy of the License at the root of this project source code
 
-use frame_system as system;
 use seed_pallet_common::test_prelude::*;
 use seed_primitives::ethy::{crypto::AuthorityId, EventProofId};
 use sp_core::ByteArray;
-use sp_runtime::{DispatchError, Percent};
+use sp_runtime::Percent;
 
 use crate as pallet_xrpl_bridge;
 
@@ -41,24 +40,7 @@ impl_frame_system_config!(Test);
 impl_pallet_balance_config!(Test);
 impl_pallet_assets_config!(Test);
 impl_pallet_timestamp_config!(Test);
-
-parameter_types! {
-	pub const AssetsExtPalletId: PalletId = PalletId(*b"assetext");
-	pub const MaxHolds: u32 = 16;
-	pub const TestParachainId: u32 = 100;
-}
-
-impl pallet_assets_ext::Config for Test {
-	type RuntimeEvent = RuntimeEvent;
-	type ParachainId = TestParachainId;
-	type MaxHolds = MaxHolds;
-	// TODO These tests should use ROOT as NativeAssetId. Changing to ROOT causes tests to fail
-	// https://futureverse.atlassian.net/jira/software/c/projects/TRN/boards/39/backlog?selectedIssue=TRN-279
-	type NativeAssetId = XrpAssetId;
-	type OnNewAssetSubscription = ();
-	type PalletId = AssetsExtPalletId;
-	type WeightInfo = ();
-}
+impl_pallet_assets_ext_config!(Test);
 
 // Time is measured by number of blocks.
 pub const MILLISECS_PER_BLOCK: u64 = 4_000;
@@ -109,9 +91,4 @@ impl XrplBridgeToEthyAdapter<AuthorityId> for MockEthyAdapter {
 			AuthorityId::from_slice(&[3_u8; 33]).unwrap(),
 		]
 	}
-}
-
-// Build genesis storage according to the mock runtime.
-pub fn new_test_ext() -> sp_io::TestExternalities {
-	system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
 }
