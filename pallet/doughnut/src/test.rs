997d84308e92c57083966152e1c88b6542dca5e6
surangap <19677661+surangap@users.noreply.github.com>
2023-12-06 14:30:35 +1300

    Add test and mock skeletons

--- pallet/doughnut/src/test.rs
+++ pallet/doughnut/src/test.rs
@@ -16,7 +16,6 @@
 use super::Event;
 use crate::{
 	mock::{RuntimeEvent as MockEvent, *},
-	Data,
 };
 use frame_support::{
 	assert_ok,
@@ -28,124 +27,11 @@ use pallet_transaction_payment::ChargeTransactionPayment;
 use seed_pallet_common::CreateExt;
 use sp_core::U256;
 use sp_runtime::{traits::SignedExtension, Perbill};
-
-#[test]
-fn charges_default_extrinsic_amount() {
-	TestExt::default().build().execute_with(|| {
-		let account = AccountId::default();
-		assert_ok!(AssetsExt::create(&account.into(), None));
-
-		let starting_fee_token_asset_balance = 4200000069;
-		assert_ok!(AssetsExt::mint_into(100, &account, starting_fee_token_asset_balance));
-
-		let fee_token_balance = Assets::balance(100, account);
-		assert_eq!(fee_token_balance, starting_fee_token_asset_balance);
-
-		assert_ok!(MockPallet::mock_charge_fee(RawOrigin::Signed(account).into()));
-
-		let call = mock_pallet::pallet::Call::mock_charge_fee {};
-		let dispatch_info = call.get_dispatch_info();
-
-		assert_ok!(<ChargeTransactionPayment<Test> as SignedExtension>::pre_dispatch(
-			ChargeTransactionPayment::from(0),
-			&account,
-			&call.into(),
-			&dispatch_info,
-			0,
-		));
-
-		let base_fee = FeeControl::weight_to_fee(
-			&BlockWeights::default().get(DispatchClass::Normal).base_extrinsic,
-		);
-		let extrinsic_fee = dispatch_info.weight.ref_time();
-
-		assert_eq!(
-			Assets::balance(100, account),
-			starting_fee_token_asset_balance - base_fee - extrinsic_fee as u128
-		);
-	});
-}
-
-#[test]
-fn charges_extrinsic_fee_based_on_setting() {
-	TestExt::default().build().execute_with(|| {
-		let account = AccountId::default();
-		assert_ok!(AssetsExt::create(&account.into(), None));
-
-		let starting_fee_token_asset_balance = 4200000069;
-
-		assert_ok!(AssetsExt::mint_into(100, &account, starting_fee_token_asset_balance));
-
-		let fee_token_balance = Assets::balance(100, account);
-		assert_eq!(fee_token_balance, starting_fee_token_asset_balance);
-		assert_ok!(MockPallet::mock_charge_fee(RawOrigin::Signed(account).into()));
-
-		assert_ok!(FeeControl::set_weight_multiplier(
-			RawOrigin::Root.into(),
-			Perbill::from_percent(42)
-		));
-
-		let call = mock_pallet::pallet::Call::mock_charge_fee {};
-		let dispatch_info = call.get_dispatch_info();
-
-		assert_ok!(<ChargeTransactionPayment<Test> as SignedExtension>::pre_dispatch(
-			ChargeTransactionPayment::from(0),
-			&account,
-			&call.into(),
-			&dispatch_info,
-			0,
-		));
-
-		let base_fee = FeeControl::weight_to_fee(
-			&BlockWeights::default().get(DispatchClass::Normal).base_extrinsic,
-		);
-		let extrinsic_fee = dispatch_info.weight.ref_time();
-
-		assert_eq!(
-			Assets::balance(100, account),
-			starting_fee_token_asset_balance - base_fee - extrinsic_fee as u128
-		);
-	});
-}
-
-#[test]
-fn set_evm_base_fee_works() {
-	TestExt::default().build().execute_with(|| {
-		let new_value = U256::from(12345);
-		assert_ok!(FeeControl::set_evm_base_fee(RawOrigin::Root.into(), new_value));
-
-		assert_eq!(Data::<Test>::get().evm_base_fee_per_gas, new_value);
-
-		System::assert_last_event(MockEvent::FeeControl(Event::<Test>::EvmBaseFeeSet {
-			base_fee: new_value,
-		}));
-	});
-}
-
-#[test]
-fn set_weight_multiplier_works() {
-	TestExt::default().build().execute_with(|| {
-		let new_value = Perbill::from_parts(12345);
-		assert_ok!(FeeControl::set_weight_multiplier(RawOrigin::Root.into(), new_value));
-
-		assert_eq!(Data::<Test>::get().weight_multiplier, new_value);
-
-		System::assert_last_event(MockEvent::FeeControl(Event::<Test>::WeightMultiplierSet {
-			weight_multiplier: new_value,
-		}));
-	});
-}
+use seed_pallet_common::test_prelude::*;
 
 #[test]
 fn set_length_multiplier_works() {
-	TestExt::default().build().execute_with(|| {
-		let new_value: Balance = 12345;
-		assert_ok!(FeeControl::set_length_multiplier(RawOrigin::Root.into(), new_value));
+	TestExt::<Test>::default().build().execute_with(|| {
 
-		assert_eq!(Data::<Test>::get().length_multiplier, new_value);
-
-		System::assert_last_event(MockEvent::FeeControl(Event::<Test>::LengthMultiplierSet {
-			length_multiplier: new_value,
-		}));
 	});
 }
