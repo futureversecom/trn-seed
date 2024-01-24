3b9f3bcf349eb7ff5b8455200ffea157cca329ab
JasonTulp <jason.j.tulp@gmail.com>
2024-01-24 16:37:40 +1300

    Add e2e test with js bindings

--- e2e/test/Doughnuts.test.ts
+++ e2e/test/Doughnuts.test.ts
@@ -1,9 +1,12 @@
 import { ApiPromise, Keyring, WsProvider } from "@polkadot/api";
-import { hexToU8a } from "@polkadot/util";
+// const Doughnut = require('@trn/doughnut-wasm').default;
+import { Doughnut } from "../../../trn-doughnut-rs/js";
+import {hexToU8a, u8aToHex} from "@polkadot/util";
+import {KeyringPair} from "@polkadot/keyring/types";
 import { expect } from "chai";
 
 import {
-    ALITH_PRIVATE_KEY,
+    ALICE_PRIVATE_KEY,
     BOB_PRIVATE_KEY,
     GAS_TOKEN_ID,
     // NodeProcess,
@@ -11,12 +14,15 @@ import {
     typedefs,
 } from "../common";
 import {Wallet} from "ethers";
+// import {Wallet} from "ethers";
 
 
 describe("Doughnuts", () => {
     // let node: NodeProcess;
     let api: ApiPromise;
     let bob: KeyringPair;
+    let bob_ecdsa: KeyringPair;
+    let alice: KeyringPair;
 
     before(async () => {
         // node = await startNode();
@@ -25,26 +31,55 @@ describe("Doughnuts", () => {
         api = await ApiPromise.create({ provider: wsProvider, types: typedefs });
 
         const keyring = new Keyring({ type: "ethereum" });
+        const keyring_ecdsa = new Keyring({ type: "ecdsa" });
         bob = keyring.addFromSeed(hexToU8a(BOB_PRIVATE_KEY));
-
+        bob_ecdsa = keyring_ecdsa.addFromSeed(hexToU8a(BOB_PRIVATE_KEY));
+        alice = keyring.addFromSeed(hexToU8a(ALICE_PRIVATE_KEY));
     });
 
     // after(async () => node.stop());
 
     it("doughnut works", async () => {
-        const receiverAddress = "0x000000000000000000000000000000000000000c";
-
-        // Doughnut from Alice to Bob
-        const doughnut = "0x011000020a1091341fe5664bfa1782d5e04779689068c916b04cb365ec3153755684d9a10390084fdbf27d2b79d26a4f13f0ccd982cb755a661969143c37cbc49ef5b91f270000000000310000000000000000000000000000000000d35aef05a023e2e75f038abba9d1357671c3044e2664c6cc099115adceb492a2677fe2233735f31442d5c33921e9d49fd305af28b8ffa6bd4ec86076ad026b8d";
-        // Bob's signature of the outer call
-        const signature = "0xbb7f00599d3b7fe0177d4478949c57151510a48bc80ac5b9868d22743cb413dd7ca06351fede444df13f4db83265eae627b54910476edb112dd8c4a5460a8309";
+        const receiverAddress = await Wallet.createRandom().getAddress();
         const transferAmount = 1234;
         const nonce = ((await api.query.system.account(bob.address)).toJSON() as any)?.nonce;
         const call = api.tx.balances.transfer(receiverAddress, transferAmount);
+        const version = 1;
+        const issuer = alice.publicKey;
+        const holder = bob.publicKey;
+        const fee_mode = 0;
+        const expiry = 100000;
+        const notBefore = 0;
+
+        console.log("\n====  Creating Doughnut");
+        const doughnut = new Doughnut(version, issuer, holder, fee_mode, expiry, notBefore);
+        // Add a random domain to allow the doughnut to be encoded
+        doughnut.addDomain("Test", new Uint8Array(12));
+        console.log(`Domain    : ${doughnut.domain("Test")}`);
+
+        // Sign the doughnut with the issuers private key
+        doughnut.signECDSA(hexToU8a(ALICE_PRIVATE_KEY));
+        console.log(`Signature : ${doughnut.signature()}`);
+
+        // Verify that the doughnut is valid
+        const verified = doughnut.verify(holder, 5);
+        expect(verified).to.be.equal(true);
+
+        // Encode the doughnut
+        const encoded_doughnut = doughnut.encode();
+        const doughnut_hex = u8aToHex(encoded_doughnut);
+
+        // Create a call with empty signature to be signed by the holder (Bob)
+        const tx = await api.tx.doughnut.transact(call, doughnut_hex, nonce, "");
+        // Convert tx to u8Array and remove the first 2 bytes (Not sure why. It's to do with length)
+        const tx_u8a = tx.toU8a(true).slice(2);
+        const tx_hex = u8aToHex(tx_u8a);
+        const signature = bob_ecdsa.sign(tx_hex);
+        const sig_hex = u8aToHex(signature);
 
         // Execute the transact call with.send
         const events = await new Promise<any[]>(async (resolve) => {
-            await api.tx.doughnut.transact(call, doughnut, nonce, signature).send(({ events = [], status }) => {
+            await api.tx.doughnut.transact(call, doughnut_hex, nonce, sig_hex).send(({ events = [], status }) => {
                 if (status.isInBlock) {
                     resolve(events);
                 }
@@ -54,7 +89,7 @@ describe("Doughnuts", () => {
         // console.log(events);
         const balance = await api.query.system.account(receiverAddress);
         const freeBalance = balance.toJSON()?.data.free;
-        console.log(`Free balance after doughnut transsact: ${freeBalance}`);
+        console.log(`Free balance after doughnut transact: ${freeBalance}`);
         expect(freeBalance).to.be.equal(transferAmount);
     });
 
