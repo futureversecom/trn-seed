e78ec49e51487e314eb054e418bd737f6bde3349
surangap <19677661+surangap@users.noreply.github.com>
2024-02-05 11:32:25 +1300

    Remove unstable e2e test.

--- e2e/test/Doughnuts.test.ts
+++ e2e/test/Doughnuts.test.ts
@@ -4,7 +4,7 @@ import { hexToU8a, u8aToHex } from "@polkadot/util";
 import { expect } from "chai";
 import { Wallet } from "ethers";
 
-import { LoadSource, OpCodeComparator, OpCodeConjunction, OpComp, OpConj, OpLoad, Pact } from "../../../../pact/js";
+import { OpCodeComparator, OpComp, OpLoad, Pact } from "../../../../pact/js";
 // const Doughnut = require('@trn/doughnut-wasm').default;
 import { Doughnut } from "../../../../trn-doughnut-rs/js";
 import { TRNNut } from "../../../../trn-trnnut-rs/js";
@@ -35,66 +35,6 @@ describe("Doughnuts", () => {
   });
 
   after(async () => node.stop());
-
-    it("doughnut works", async () => {
-        const receiverAddress = await Wallet.createRandom().getAddress();
-        const transferAmount = 1234;
-        const nonce = ((await api.query.system.account(bob.address)).toJSON() as any)?.nonce;
-        const call = api.tx.balances.transfer(receiverAddress, transferAmount);
-        const version = 1;
-        const issuer = alice.publicKey;
-        const holder = bob.publicKey;
-        const fee_mode = 0;
-        const expiry = 100000;
-        const notBefore = 0;
-
-        console.log("\n====  Creating Doughnut");
-        const doughnut = new Doughnut(version, issuer, holder, fee_mode, expiry, notBefore);
-        // Add a random domain to allow the doughnut to be encoded
-        doughnut.addDomain("Test", new Uint8Array(12));
-        console.log(`Domain    : ${doughnut.domain("Test")}`);
-
-        // Sign the doughnut with the issuers private key
-        doughnut.signECDSA(hexToU8a(ALICE_PRIVATE_KEY));
-        console.log(`Signature : ${doughnut.signature()}`);
-
-        // Verify that the doughnut is valid
-        const verified = doughnut.verify(holder, 5);
-        expect(verified).to.be.equal(true);
-
-        // Encode the doughnut
-        const encoded_doughnut = doughnut.encode();
-        const doughnut_hex = u8aToHex(encoded_doughnut);
-
-        // Create a call with empty signature to be signed by the holder (Bob)
-        const tx = await api.tx.doughnut.transact(call, doughnut_hex, nonce, "");
-        // Convert tx to u8Array and remove the first 2 bytes (Not sure why. It's to do with length)
-        const tx_u8a = tx.toU8a(true).slice(2);
-        const tx_hex = u8aToHex(tx_u8a);
-        const signature = bob_ecdsa.sign(tx_hex);
-        const sig_hex = u8aToHex(signature);
-
-        // Execute the transact call with.send
-        const eventData = await new Promise<any[]>((resolve, reject) => {
-            api.tx.doughnut.transact(call, doughnut_hex, nonce, sig_hex).send(({ events, status }) => {
-                if (status.isInBlock) {
-                    for (const { event } of events) {
-                        if (event.section === "doughnut" && event.method === "DoughnutCallExecuted") {
-                            resolve(event.data);
-                        }
-                    }
-                    reject(null);
-                }
-            });
-        });
-        expect(eventData).to.exist;
-
-        // console.log(events);
-        const balance = await api.query.system.account(receiverAddress);
-        const freeBalance = balance.toJSON()?.data.free;
-        console.log(`Free balance after doughnut transact: ${freeBalance}`);
-        expect(freeBalance).to.be.equal(transferAmount);
-    });
 
   it("doughnut works - alice issued doughnut for Balances::transfer with constraints amount = 10", async () => {
     const receiverAddress = await Wallet.createRandom().getAddress();
@@ -327,68 +267,63 @@ describe("Doughnuts", () => {
     // check the events
   });
 
-  it("permissioned doughnut works", async () => {
-    const receiverAddress = "0x000000000000000000000000000000000000000c";
-
-    // Doughnut from Alice to Bob
-    const doughnut =
-      "0x011000020a1091341fe5664bfa1782d5e04779689068c916b04cb365ec3153755684d9a10390084fdbf27d2b79d26a4f13f0ccd982cb755a661969143c37cbc49ef5b91f27000000000074726e0000000000000000000000000046000000000042616c616e636573000000000000000000000000000000000000000000000000007472616e73666572000000000000000000000000000000000000000000000000005f305526a01810151e53cf8d40565770606b08b2de9304165033a9a09c6c81af6d0905b02a5768f758821d47a024123612e0be83ea69ba43114dff4eaa10ea31";
-    // Bob's signature of the outer call
-    const signature =
-      "0x89428293041e0f6e747fe3775f8770a133ea24e54a8b9bcd1528c515b35b9965354a5074a93d2e191065d69fa6438bfc3d32147deb16a357e99bbf644243c3fe";
+  it("doughnut without trn permission object fails", async () => {
+    const receiverAddress = await Wallet.createRandom().getAddress();
     const transferAmount = 1234;
     const nonce = ((await api.query.system.account(bob.address)).toJSON() as any)?.nonce;
     const call = api.tx.balances.transfer(receiverAddress, transferAmount);
+    const version = 1;
+    const issuer = alice.publicKey;
+    const holder = bob.publicKey;
+    const fee_mode = 0;
+    const expiry = 100000;
+    const notBefore = 0;
+
+    console.log("\n====  Creating Doughnut");
+    const doughnut = new Doughnut(version, issuer, holder, fee_mode, expiry, notBefore);
+    // Add a random domain to allow the doughnut to be encoded
+    doughnut.addDomain("Test", new Uint8Array(12));
+    console.log(`Domain    : ${doughnut.domain("Test")}`);
+
+    // Sign the doughnut with the issuers private key
+    doughnut.signECDSA(hexToU8a(ALICE_PRIVATE_KEY));
+    console.log(`Signature : ${doughnut.signature()}`);
+
+    // Verify that the doughnut is valid
+    const verified = doughnut.verify(holder, 5);
+    expect(verified).to.be.equal(true);
+
+    // Encode the doughnut
+    const encoded_doughnut = doughnut.encode();
+    const doughnut_hex = u8aToHex(encoded_doughnut);
+
+    // Create a call with empty signature to be signed by the holder (Bob)
+    const tx = await api.tx.doughnut.transact(call, doughnut_hex, nonce, "");
+    // Convert tx to u8Array and remove the first 2 bytes (Not sure why. It's to do with length)
+    const tx_u8a = tx.toU8a(true).slice(2);
+    const tx_hex = u8aToHex(tx_u8a);
+    const signature = bob_ecdsa.sign(tx_hex);
+    const sig_hex = u8aToHex(signature);
+
+    // alice balance before
+    const alice_balance_before = await api.query.system.account(alice.address);
 
     // Execute the transact call with.send
-    const events = await new Promise<any[]>(async (resolve) => {
-      await api.tx.doughnut.transact(call, doughnut, nonce, signature).send(({ events = [], status }) => {
-        if (status.isInBlock) {
-          resolve(events);
-        }
+    await api.tx.doughnut
+      .transact(call, doughnut_hex, nonce, sig_hex)
+      .send()
+      .catch((err: any) => {
+        console.log(err);
       });
-    });
 
     // console.log(events);
     const balance = await api.query.system.account(receiverAddress);
     const freeBalance = balance.toJSON()?.data.free;
-    console.log(`Free balance after doughnut transsact: ${freeBalance}`);
-    expect(freeBalance).to.be.equal(transferAmount);
-  });
+    console.log(`Free balance after doughnut transact: ${freeBalance}`);
+    expect(freeBalance).to.be.equal(0);
 
-  it("permissioned doughnut does not allow other extrinsics to pass through", async () => {
-    const receiverAddress = "0x000000000000000000000000000000000000000c";
-
-    // Doughnut from Alice to Bob
-    const doughnut =
-      "0x011000020a1091341fe5664bfa1782d5e04779689068c916b04cb365ec3153755684d9a10390084fdbf27d2b79d26a4f13f0ccd982cb755a661969143c37cbc49ef5b91f27000000000074726e0000000000000000000000000046000000000042616c616e636573000000000000000000000000000000000000000000000000007472616e73666572000000000000000000000000000000000000000000000000005f305526a01810151e53cf8d40565770606b08b2de9304165033a9a09c6c81af6d0905b02a5768f758821d47a024123612e0be83ea69ba43114dff4eaa10ea31";
-    // Bob's signature of the outer call
-    const signature =
-      "0xafbd9cdf96458caf2c3817fd749e0d3f901d84a38e2e32265e8f6251f16a15c8577fbe297c4349658af31945a4508609a214eb426f4bb8b8647a398e62c953c4";
-    const transferAmount = 1234;
-    const nonce = ((await api.query.system.account(bob.address)).toJSON() as any)?.nonce;
-    const call = api.tx.balances.transferKeepAlive(receiverAddress, transferAmount);
-
-    const aliceBalanceBefore = await api.query.system.account("0xe04cc55ebee1cbce552f250e85c57b70b2e2625b");
-    const aliceBalanceBeforeFree = aliceBalanceBefore.toJSON()?.data.free;
-
-    // Execute the transact call with.send
-    const events = await new Promise<any[]>(async (resolve) => {
-      await api.tx.doughnut.transact(call, doughnut, nonce, signature).send(({ events = [], status }) => {
-        if (status.isInBlock) {
-          resolve(events);
-        }
-      });
-    });
-
-    // console.log(events);
-    const balance = await api.query.system.account(receiverAddress);
-    const freeBalance = balance.toJSON()?.data.free;
-    console.log(`Free balance after doughnut transsact: ${freeBalance}`);
-    expect(freeBalance).to.be.equal(0); // inner call did not get executed.
-
-    const aliceBalanceAfter = await api.query.system.account("0xe04cc55ebee1cbce552f250e85c57b70b2e2625b");
-    const aliceBalanceAfterFree = aliceBalanceAfter.toJSON()?.data.free;
-    expect(aliceBalanceBeforeFree - aliceBalanceAfterFree).to.be.lessThan(transferAmount); // Alice only pays for the fee.
+    const alice_balance_after = await api.query.system.account(alice.address);
+    // alice should bear transferAmount in Root + fees in XRP
+    expect(alice_balance_before.toJSON()?.data.free - alice_balance_after.toJSON()?.data.free).equal(0);
   });
 });
