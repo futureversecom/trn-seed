ee100f86b12cb9217414efa1528d2f1f55aa6054
zees-dev <dev.786zshan@gmail.com>
2024-02-22 13:26:53 +1300

    initialized TRN crowdsale pallet

---
+++ pallet/crowdsale/src/types.rs
@@ -0,0 +1,32 @@
+use codec::{Decode, Encode, MaxEncodedLen};
+use frame_support::sp_runtime::RuntimeDebug;
+use scale_info::TypeInfo;
+use seed_primitives::{AccountId, AssetId, Balance, BlockNumber, CollectionUuid};
+use sp_core::U256;
+
+#[derive(Clone, Copy, Encode, Decode, RuntimeDebug, PartialEq, Eq, TypeInfo, MaxEncodedLen)]
+pub struct SaleInformation<AccountId, BlockNumber> {
+	pub status: SaleStatus,
+	pub admin: AccountId,
+	pub payment_asset: AssetId,
+	pub collection_id: CollectionUuid,
+	pub tokens_per_nft: Balance,
+	pub funds_raised: Balance,
+	pub start_block: BlockNumber,
+	pub end_block: BlockNumber,
+}
+
+#[derive(Clone, Copy, Encode, Decode, RuntimeDebug, PartialEq, Eq, TypeInfo, MaxEncodedLen)]
+pub enum SaleStatus {
+	Disabled,
+	Enabled,
+	Paying,
+	Refunding,
+	Closed,
+}
+
+impl Default for SaleStatus {
+	fn default() -> Self {
+		SaleStatus::Disabled
+	}
+}
