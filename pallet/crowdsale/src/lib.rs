877edbd704ad4decfc7e07924f3183228908c7b2
zees-dev <dev.786zshan@gmail.com>
2024-02-27 12:34:40 +1300

    Pallet crowdsale upd (#795)
    * added sp io dep
    
    * - updated SaleInformation to add vault account
    - updated SaleStatus to also display block num with status variants
    
    * - updated storage types
    - updated events and errors
    - added code for offchain worker
    - added claim function
    - refactor logic
    
    * SaleEndBlocks back to option query, payout admin account payment_asset in on_initialize

--- pallet/crowdsale/src/lib.rs
+++ pallet/crowdsale/src/lib.rs
@@ -21,6 +21,7 @@
 //! If softcap is not reached, the difference is refunded to the crowdsale creator.
 //! The vouchers can be used to redeemed for NFTs from the collection.
 #![cfg_attr(not(feature = "std"), no_std)]
+extern crate alloc;
 
 pub use pallet::*;
 
@@ -30,14 +31,18 @@ use frame_support::{
 		traits::{AccountIdConversion, One, Zero},
 		SaturatedConversion, Saturating,
 	},
-	traits::fungibles::{self, Inspect, InspectMetadata, Mutate, Transfer},
+	traits::fungibles::{self, Inspect, Mutate, Transfer},
 	transactional, PalletId,
 };
-use frame_system::pallet_prelude::*;
+use frame_system::{
+	offchain::{SendTransactionTypes, SubmitTransaction},
+	pallet_prelude::*,
+};
 use pallet_nft::traits::NFTExt;
-use seed_pallet_common::{CreateExt, InspectExt};
-use seed_primitives::{AccountId, AssetId, Balance, BlockNumber, CollectionUuid, TokenCount};
-use sp_core::U256;
+use seed_pallet_common::{log, CreateExt, InspectExt};
+use seed_primitives::{
+	AccountId, AssetId, Balance, BlockNumber, CollectionUuid, OffchainErr, TokenCount,
+};
 use sp_std::vec;
 
 pub mod types;
@@ -58,6 +63,9 @@ mod tests;
 #[allow(dead_code)]
 pub(crate) const LOG_TARGET: &str = "crowdsale";
 
+pub const CROWDSALE_DIST_UNSIGNED_PRIORITY: TransactionPriority =
+	TransactionPriority::max_value() / 2;
+
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
@@ -67,7 +75,7 @@ pub mod pallet {
 	pub struct Pallet<T>(_);
 
 	#[pallet::config]
-	pub trait Config: frame_system::Config {
+	pub trait Config: frame_system::Config + SendTransactionTypes<Call<Self>> {
 		/// The system event type
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 
@@ -103,10 +111,13 @@ pub mod pallet {
 		StorageMap<_, Twox64Concat, SaleId, SaleInformation<T::AccountId, T::BlockNumber>>;
 
 	/// User participation in the sale
+	/// sale_id -> user -> payment_asset contribution amount
 	#[pallet::storage]
 	pub type SaleParticipation<T: Config> =
-		StorageMap<_, Twox64Concat, (SaleId, T::AccountId), Balance, ValueQuery>;
+		StorageDoubleMap<_, Twox64Concat, SaleId, Twox64Concat, T::AccountId, Balance, OptionQuery>;
 
+	/// Map from block number to the sales that will end at that block
+	/// The tuple represents the sale id and the current sale participant distribution index
 	#[pallet::storage]
 	pub type SaleEndBlocks<T: Config> = StorageMap<
 		_,
@@ -136,6 +147,10 @@ pub mod pallet {
 			collection_id: CollectionUuid,
 			quantity: TokenCount,
 		},
+		/// Crowdsale closed
+		CrowdsaleClosed { id: SaleId, info: SaleInformation<T::AccountId, T::BlockNumber> },
+		/// Crowdsale vouchers claimed
+		CrowdsaleVouchersClaimed { id: SaleId, who: T::AccountId, amount: Balance },
 	}
 
 	#[pallet::error]
@@ -178,20 +193,69 @@ pub mod pallet {
 		ManualStartDisabled,
 		/// There are too many sales queued for this block, try again on a different block
 		TooManySales,
-		/// Can't close the sale as the sale is not enabled
-		SaleNotEnabled,
+		/// Vouchers have already been claimed
+		VouchersAlreadyClaimed,
 	}
 
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
 		/// Check and close all expired listings
 		fn on_initialize(now: T::BlockNumber) -> Weight {
-			let _total_closed = Self::close_sales_at(now);
+			match Self::close_sales_at(now) {
+				Ok(total_closed) =>
+					log!(info, "✅ closed {} sales at block {:?}", total_closed, now),
+				Err(e) => log!(error, "⛔️ failed to close sales at block {:?}: {:?}", now, e),
+			};
 			// TODO Benchmark this
 			// <T as Config>::WeightInfo::close().mul(total_closed as u64)
 			// total_closed == 1 read + 1 write per close
 			// + 1 read + write for SaleEndBlocks
 			Weight::zero()
+		}
+
+		/// Offchain worker processes closed sales to distribute voucher rewards to participants
+		fn offchain_worker(now: T::BlockNumber) {
+			if !sp_io::offchain::is_validator() {
+				log!(
+					error,
+					"⛔️ offchain worker error at block [{:?}]: {:?}",
+					now,
+					OffchainErr::NotAValidator
+				);
+			}
+
+			if SaleEndBlocks::<T>::get(&now).is_some() {
+				log!(info, "⭐️ distributing rewards for crowdsales closing at {:?}", now);
+				let call = Call::distribute_crowdsale_rewards { block: now };
+				let _ = SubmitTransaction::<T, Call<T>>::submit_unsigned_transaction(call.into());
+			}
+		}
+	}
+
+	#[pallet::validate_unsigned]
+	impl<T: Config> ValidateUnsigned for Pallet<T> {
+		type Call = Call<T>;
+
+		fn validate_unsigned(_source: TransactionSource, call: &Self::Call) -> TransactionValidity {
+			match call {
+				Call::distribute_crowdsale_rewards { block } => {
+					// reject crowdsale distribution tx which have already been processed
+					let now = <frame_system::Pallet<T>>::block_number();
+					if SaleEndBlocks::<T>::get(&now).is_none() {
+						return InvalidTransaction::Stale.into()
+					}
+					if now < *block {
+						return InvalidTransaction::Future.into()
+					}
+					ValidTransaction::with_tag_prefix("CrowdsaleDistOffchainWorker")
+						.priority(CROWDSALE_DIST_UNSIGNED_PRIORITY)
+						.and_provides(now)
+						.longevity(64_u64)
+						.propagate(true)
+						.build()
+				},
+				_ => InvalidTransaction::Call.into(),
+			}
 		}
 	}
 
@@ -241,14 +305,11 @@ pub mod pallet {
 			// ensure soft_cap_price is not zero - prevent future div by zero
 			ensure!(!soft_cap_price.is_zero(), Error::<T>::InvalidAsset);
 
-			// TODO, maybe set the max issuance here?
-			// Might be bad user experience to create a collection and fail due to one of these 2
-			// Potentially a better approach would be to create the collection here.
-			// Discuss with Zee
-			// What happens if we set the max issuance after the sale? If the vouchers
-			// are reliant on the max issuance, the sale will end up corrupt
+			// create crowdsale vault account which will temporary manage ownership and hold funds
+			let vault = Self::vault_account(sale_id);
 
-			// TODO: pass ownership to the pallet account
+			// TODO: pass NFT collection ownership to the vault account
+			// - this is required so collection owner cannot mint/rug to dilute the crowdsale
 
 			let collection_info = T::NFTExt::get_collection_info(collection_id)?;
 			ensure!(collection_info.max_issuance.is_some(), Error::<T>::MaxIssuanceNotSet);
@@ -258,13 +319,13 @@ pub mod pallet {
 			);
 
 			// create voucher asset
-			let voucher_decimals = T::MultiCurrency::decimals(&payment_asset);
-			let voucher_asset_id = Self::create_voucher_asset(sale_id)?;
+			let voucher_asset_id = Self::create_voucher_asset(&vault, sale_id)?;
 
 			// store the sale information
 			let sale_info = SaleInformation::<T::AccountId, T::BlockNumber> {
 				status: SaleStatus::Disabled,
 				admin: who.clone(),
+				vault,
 				payment_asset,
 				reward_collection_id: collection_id,
 				soft_cap_price,
@@ -278,10 +339,11 @@ pub mod pallet {
 			Ok(())
 		}
 
-		// TODO Should we have the sale start automatically at the start block?
-		/// Enable a crowdsale if current block within range of the start and end block.
-		/// This will enable the sale to be participated in.
-		/// Any user can call this function to initialize the sale once block conditions are met.
+		/// Enable a crowdsale for user participation.
+		/// Only the crowdsale admin can call this function to enable the sale.
+		/// This will enable the sale to be participated in by any user which has required
+		/// payment_asset. The sale will be closed automatically once the sale_duration is met; the
+		/// sale end block/time is based on current block + sale_duration.
 		///
 		/// Parameters:
 		/// - `id`: The id of the sale to enable
@@ -296,7 +358,7 @@ pub mod pallet {
 			SaleInfo::<T>::try_mutate(id, |sale_info| -> DispatchResult {
 				let Some(sale_info) = sale_info else {
 					return Err(Error::<T>::CrowdsaleNotFound.into());
-        		};
+				};
 
 				// ensure the sale is not already enabled
 				ensure!(
@@ -319,8 +381,9 @@ pub mod pallet {
 					}
 					Ok(())
 				})?;
-				// update the sale status
-				sale_info.status = SaleStatus::Enabled;
+
+				// update the sale details
+				sale_info.status = SaleStatus::Enabled(<frame_system::Pallet<T>>::block_number());
 
 				Self::deposit_event(Event::CrowdsaleEnabled {
 					id,
@@ -356,23 +419,30 @@ pub mod pallet {
 				};
 
 				// ensure the sale is enabled
-				ensure!(sale_info.status == SaleStatus::Enabled, Error::<T>::CrowdsaleNotEnabled);
+				ensure!(
+					matches!(sale_info.status, SaleStatus::Enabled(_)),
+					Error::<T>::CrowdsaleNotEnabled
+				);
 
-				// transfer payment tokens to the pallet account
-				let pallet_account = T::PalletId::get().into_account_truncating();
+				// transfer payment tokens to the crowdsale vault
 				T::MultiCurrency::transfer(
 					sale_info.payment_asset,
 					&who,
-					&pallet_account,
+					&sale_info.vault,
 					amount,
 					false,
 				)
 				.map_err(|_| Error::<T>::AssetTransferFailed)?;
 
-				// update the sale status
+				// update the sale funds
 				sale_info.funds_raised = sale_info.funds_raised.saturating_add(amount);
-				SaleParticipation::<T>::mutate((id, who.clone()), |participation| {
-					*participation = participation.saturating_add(amount)
+
+				// update the user's contribution
+				SaleParticipation::<T>::mutate(id, who.clone(), |maybe_contribute| {
+					match maybe_contribute {
+						Some(contribution) => *contribution = contribution.saturating_add(amount),
+						None => *maybe_contribute = Some(amount),
+					}
 				});
 
 				Self::deposit_event(Event::CrowdsaleParticipated {
@@ -380,6 +450,111 @@ pub mod pallet {
 					who,
 					asset: sale_info.payment_asset,
 					amount,
+				});
+
+				Ok(())
+			})?;
+
+			Ok(())
+		}
+
+		/// Distribute vouchers for a given crowdsale - based on the amount of funds raised, the NFT
+		/// collection max issuance and respective participant contributions.
+		/// The crowdsale must be closed (automated on chain via `on_initialize`).
+		/// The extrinsic is automatically called by offchain worker once the sale is closed (end
+		/// block reached). The extrinsic can also manually be called by anyone.
+		///
+		/// Parameters:
+		/// - `id`: The id of the sale to distribute the vouchers for
+		///
+		/// Emits `CrowdsaleVouchersDistributed` event when successful.
+		#[pallet::weight(0)]
+		#[transactional]
+		pub fn distribute_crowdsale_rewards(
+			origin: OriginFor<T>,
+			block: T::BlockNumber,
+		) -> DispatchResult {
+			// let _who = ensure_signed(origin)?; // TODO: allow this to be callable by anyone
+
+			ensure_none(origin)?;
+			// ensure_none(origin)?;
+
+			// TODO: get the sale info
+			// TODO: get sale end_block: (SaleStatus::enabled(start_block)) + the sale_duration
+			// TODO: get the sale index from list in the SaleEndBlocks
+
+			// TODO: get distribution index; if sale closed, index = 0, else index =
+			// SaleStatus::Distributing(index) TODO: update sale participant distribution index; if
+			// fully processed, update SaleStatus::Distributed TODO: remove sale from SaleEndBlocks
+			// list if fully processed (SaleStatus::Distributed) TODO: emit event for each
+			// distribution
+
+			Ok(())
+		}
+
+		/// Claim the vouchers after a sale has concluded, based on caller's contribution.
+		/// The vouchers are redeemable 1:1 with the NFTs in the collection (excluding decimals).
+		/// A successful claim will remove the user's contribution from the sale and mint the
+		/// vouchers to the user.
+		///
+		/// Parameters:
+		/// - `id`: The id of the sale to claim the vouchers from
+		///
+		/// Emits `CrowdsaleVouchersClaimed` event when successful.
+		#[pallet::weight(0)]
+		#[transactional]
+		pub fn claim(origin: OriginFor<T>, id: SaleId) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+
+			SaleInfo::<T>::try_mutate(id, |sale_info| -> DispatchResult {
+				let Some(sale_info) = sale_info else {
+					return Err(Error::<T>::CrowdsaleNotFound.into());
+				};
+
+				// ensure the sale is closed
+				ensure!(
+					matches!(sale_info.status, SaleStatus::Closed(_)),
+					Error::<T>::InvalidCrowdsaleStatus
+				);
+
+				// mint vouchers to user based on contribution; remove user from sale
+				let contribution = SaleParticipation::<T>::take(id, &who)
+					.ok_or(Error::<T>::VouchersAlreadyClaimed)?; // TODO: fix error
+
+				// get amount of claimable vouchers based on the user's contribution
+				let collection_info =
+					T::NFTExt::get_collection_info(sale_info.reward_collection_id)?;
+				let voucher_total_supply =
+					collection_info.max_issuance.ok_or(Error::<T>::MaxIssuanceNotSet)?;
+
+				// calculate the claimable vouchers
+				let claimable_vouchers = 0;
+
+				// TODO: uncomment this once calculate_voucher_rewards is implemented
+				// let claimable_vouchers = Self::calculate_voucher_rewards(
+				// sale_info.soft_cap_price,
+				// sale_info.funds_raised,
+				// contribution,
+				// voucher_total_supply.into(),
+				//
+				// ).map_err(|e|
+				// 	log!(
+				// 		error,
+				// 		"⛔️ failed to calculate voucher rewards for user {:?} in sale {:?}: {:?}",
+				// 		who,
+				// 		id,
+				// 		e
+				// 	);
+				// 	Error::<T>::VoucherClaimFailed)
+				// )?;
+
+				// mint claimable vouchers to the user
+				T::MultiCurrency::mint_into(sale_info.voucher, &who, claimable_vouchers)?;
+
+				Self::deposit_event(Event::CrowdsaleVouchersClaimed {
+					id,
+					who,
+					amount: claimable_vouchers,
 				});
 
 				Ok(())
@@ -402,44 +577,40 @@ pub mod pallet {
 		pub fn redeem(origin: OriginFor<T>, id: SaleId, quantity: TokenCount) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 
-			SaleInfo::<T>::try_mutate(
-				id,
-				|sale_info: &mut Option<SaleInformation<_, _>>| -> DispatchResult {
-					let Some(sale_info) = sale_info else {
+			SaleInfo::<T>::try_mutate(id, |sale_info| -> DispatchResult {
+				let Some(sale_info) = sale_info else {
 					return Err(Error::<T>::CrowdsaleNotFound.into());
 				};
 
-					// ensure the sale has concluded
-					ensure!(
-						sale_info.status == SaleStatus::Closed,
-						Error::<T>::InvalidCrowdsaleStatus
-					);
+				// ensure the sale has concluded
+				ensure!(
+					matches!(sale_info.status, SaleStatus::Closed(_)),
+					Error::<T>::InvalidCrowdsaleStatus
+				);
 
-					// burn vouchers from the user, will fail if the user does not have enough
-					// vouchers since 1:1 mapping between vouchers and NFTs, we can use the quantity
-					// * decimals as the amount burned
-					let voucher_amount =
-						quantity.saturating_mul(10u32.pow(VOUCHER_DECIMALS as u32));
-					T::MultiCurrency::burn_from(sale_info.voucher, &who, voucher_amount.into())?;
+				// burn vouchers from the user, will fail if the user does not have enough
+				// vouchers since 1:1 mapping between vouchers and NFTs, we can use the quantity
+				// * decimals as the amount burned
+				let voucher_amount = quantity.saturating_mul(10u32.pow(VOUCHER_DECIMALS as u32));
+				T::MultiCurrency::burn_from(sale_info.voucher, &who, voucher_amount.into())?;
 
-					// mint the NFT(s) to the user
-					T::NFTExt::do_mint(
-						T::PalletId::get().into_account_truncating(),
-						sale_info.reward_collection_id,
-						quantity,
-						Some(who.clone()),
-					)?;
+				// mint the NFT(s) to the user
+				T::NFTExt::do_mint(
+					T::PalletId::get().into_account_truncating(),
+					sale_info.reward_collection_id,
+					quantity,
+					Some(who.clone()),
+				)?;
 
-					Self::deposit_event(Event::CrowdsaleNFTRedeemed {
-						id,
-						who,
-						collection_id: sale_info.reward_collection_id,
-						quantity,
-					});
+				Self::deposit_event(Event::CrowdsaleNFTRedeemed {
+					id,
+					who,
+					collection_id: sale_info.reward_collection_id,
+					quantity,
+				});
 
-					Ok(())
-				},
-			)?;
+				Ok(())
+			})?;
 
 			Ok(())
 		}
