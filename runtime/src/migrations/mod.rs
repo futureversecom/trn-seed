fb839e4fdb123461b152d3c28d12453be69c159f
JasonTulp <jason.j.tulp@gmail.com>
2024-03-18 14:12:12 +1300

    Add storage migration for participations

--- runtime/src/migrations/mod.rs
+++ runtime/src/migrations/mod.rs
@@ -13,6 +13,8 @@
 // limitations under the License.
 // You may obtain a copy of the License at the root of this project source code
 
+mod crowdsale;
+
 use codec::{Decode, Encode, FullCodec, FullEncode};
 use frame_support::{
 	migration::{
@@ -30,16 +32,16 @@ pub struct AllMigrations;
 impl OnRuntimeUpgrade for AllMigrations {
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<Vec<u8>, &'static str> {
-		Ok(Vec::<u8>::new())
+		crowdsale::Upgrade::pre_upgrade()
 	}
 
 	fn on_runtime_upgrade() -> Weight {
-		Weight::from_ref_time(0_u64)
+		crowdsale::Upgrade::on_runtime_upgrade()
 	}
 
 	#[cfg(feature = "try-runtime")]
 	fn post_upgrade(_state: Vec<u8>) -> Result<(), &'static str> {
-		Ok(())
+		crowdsale::Upgrade::post_upgrade(state)
 	}
 }
 
