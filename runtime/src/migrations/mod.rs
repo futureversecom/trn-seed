f6f2f0d4c206006e89672cbee0a277b5d8e7112d
JasonTulp <jason.j.tulp@gmail.com>
2024-03-27 15:59:35 +1300

    Fix migration

--- runtime/src/migrations/mod.rs
+++ runtime/src/migrations/mod.rs
@@ -13,8 +13,7 @@
 // limitations under the License.
 // You may obtain a copy of the License at the root of this project source code
 
-mod crowdsale;
-mod erc20Peg;
+mod erc20_peg;
 
 use codec::{Decode, Encode, FullCodec, FullEncode};
 use frame_support::{
@@ -33,18 +32,16 @@ pub struct AllMigrations;
 impl OnRuntimeUpgrade for AllMigrations {
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<Vec<u8>, &'static str> {
-		erc20Peg::Upgrade::pre_upgrade();
-		crowdsale::Upgrade::pre_upgrade()
+		erc20_peg::Upgrade::pre_upgrade()
 	}
 
 	fn on_runtime_upgrade() -> Weight {
-		crowdsale::Upgrade::on_runtime_upgrade()
+		erc20_peg::Upgrade::on_runtime_upgrade()
 	}
 
 	#[cfg(feature = "try-runtime")]
 	fn post_upgrade(state: Vec<u8>) -> Result<(), &'static str> {
-		erc20Peg::Upgrade::post_upgrade(state);
-		crowdsale::Upgrade::post_upgrade(state)
+		erc20_peg::Upgrade::post_upgrade(state)
 	}
 }
 
