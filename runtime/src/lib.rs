ba30141da49cf6346e85d5bdddcbb36efc97b077
JasonT <50426640+JasonTulp@users.noreply.github.com>
2024-05-16 13:37:07 +1200

    Update spec_version to 54 (#842)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -153,10 +153,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("root"),
 	impl_name: create_runtime_str!("root"),
 	authoring_version: 1,
-	spec_version: 53,
+	spec_version: 54,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 8,
+	transaction_version: 9,
 	state_version: 0,
 };
 
