88ecba4cec7e38ebf6feebe3df1334cc6fcb7a97
zees-dev <dev.786zshan@gmail.com>
2024-05-31 10:07:39 +1200

    pallet-assets-ext-benchmarks

--- pallet/assets-ext/src/benchmarking.rs
+++ pallet/assets-ext/src/benchmarking.rs
@@ -31,6 +31,13 @@ pub fn origin<T: Config>(acc: &T::AccountId) -> RawOrigin<T::AccountId> {
 }
 
 benchmarks! {
+
+	set_asset_deposit {
+	}: _(RawOrigin::Root, One::one())
+	verify {
+		assert_eq!(AssetDeposit::<T>::get(), One::one());
+	}
+
 	create_asset {
 		let alice = account::<T>("Alice");
 		assert_ok!(AssetsExt::<T>::mint_into(T::NativeAssetId::get(), &alice, 1000000u32.into()));
@@ -41,10 +48,34 @@ benchmarks! {
 		let owner = Some(alice.clone());
 	}: _(origin::<T>(&alice), name, symbol, decimals, min_balance, owner)
 
-	set_asset_deposit {
-	}: _(RawOrigin::Root, One::one())
+	mint {
+		let alice = account::<T>("Alice");
+		let account_lookup = T::Lookup::unlookup(alice.clone());
+		let usdc = AssetsExt::<T>::create(&alice, None).unwrap();
+	}: _(origin::<T>(&alice), usdc, account_lookup, 1_500_000)
 	verify {
-		assert_eq!(AssetDeposit::<T>::get(), One::one());
+		assert_eq!(AssetsExt::<T>::balance(usdc, &alice), 1_500_000);
+	}
+
+	transfer {
+		let alice = account::<T>("Alice");
+		let usdc = AssetsExt::<T>::create(&alice, None).unwrap();
+		let bob = account::<T>("Bob");
+		let account_lookup = T::Lookup::unlookup(bob.clone());
+		assert_ok!(AssetsExt::<T>::mint_into(usdc, &alice, 1_500_000u32.into()));
+	}: _(origin::<T>(&alice), usdc, bob.clone(), 1_500_000, false)
+	verify {
+		assert_eq!(AssetsExt::<T>::balance(usdc, &alice), 0);
+		assert_eq!(AssetsExt::<T>::balance(usdc, &bob), 1_500_000);
+	}
+
+	burn_from {
+		let alice = account::<T>("Alice");
+		let usdc = AssetsExt::<T>::create(&alice, None).unwrap();
+		assert_ok!(AssetsExt::<T>::mint_into(usdc, &alice, 1_500_000u32.into()));
+	}: _(origin::<T>(&alice), usdc, T::Lookup::unlookup(alice.clone()), 1_000_000)
+	verify {
+		assert_eq!(AssetsExt::<T>::balance(usdc, &alice), 500_000);
 	}
 }
 
