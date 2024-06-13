8489645fc763d065ced2e57586651cb6c52581a4
leo <leo@plugdefi.io>
2024-06-13 22:33:42 +1200

    Add Vortex distribution era setting and asset management in benchmarking and tests
    - Introduced setting of distribution eras in benchmarking.
    - Implemented minting and management of assets in the distribution process.
    - Adjusted asset price settings and verified correctness.
    - Enhanced test coverage with additional assertions and scenarios.
    - Updated various asset-related balances and distribution statuses in tests.

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1350,6 +1350,7 @@ impl pallet_futurepass::Config for Runtime {
 }
 
 parameter_types! {
+	pub const VtxHeldPotId: PalletId = PalletId(*b"vtx/hpot");
 	pub const VtxVortexPotId: PalletId = PalletId(*b"vtx/vpot");
 	pub const VtxRootPotId: PalletId = PalletId(*b"vtx/rpot");
 	pub const VtxTxFeePotId: PalletId = PalletId(*b"vtx/fpot");
@@ -1366,6 +1367,7 @@ impl pallet_vortex_distribution::Config for Runtime {
 	type WeightInfo = weights::pallet_vortex_distribution::WeightInfo<Runtime>;
 	type NativeAssetId = RootAssetId;
 	type VtxAssetId = VortexAssetId;
+	type VtxHeldPotId = VtxHeldPotId;
 	type VtxDistPotId = VtxVortexPotId;
 	type RootPotId = VtxRootPotId;
 	type TxFeePotId = VtxTxFeePotId;
