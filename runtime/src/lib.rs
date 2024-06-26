3c257fc85aa2bd87ea6c09e2bde73aa7da5a2c90
surangap <19677661+surangap@users.noreply.github.com>
2024-06-27 11:30:34 +1200

    Update the BLOCK_GAS_LIMIT to 14_800_000

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1075,7 +1075,8 @@ impl pallet_evm_chain_id::Config for Runtime {
 }
 
 // Start frontier/EVM stuff
-const BLOCK_GAS_LIMIT: u64 = 150_000_000;
+// Number suitable for TRN, based on the gas benchmarks on current standard spec.
+const BLOCK_GAS_LIMIT: u64 = 14_800_000;
 // Default value from Frontier
 const MAX_POV_SIZE: u64 = 5 * 1024 * 1024;
 
