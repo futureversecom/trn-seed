2c2bc5818e0440bb2012a29aa57d7f0c7c1b3e4a
JasonTulp <jason.j.tulp@gmail.com>
2024-12-06 12:36:11 +1300

    update spec version and transaction version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -158,10 +158,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("root"),
 	impl_name: create_runtime_str!("root"),
 	authoring_version: 1,
-	spec_version: 62,
+	spec_version: 63,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 12,
+	transaction_version: 13,
 	state_version: 0,
 };
 
