4bb319d8c8946b4e957a048925ec82e63897915c
John Carlo San Pedro <j.carlosanpedro@gmail.com>
2024-12-16 17:56:28 +1300

    implement pallet functionality for registering resolvers

--- pallet/sylo/src/lib.rs
+++ pallet/sylo/src/lib.rs
@@ -42,40 +42,162 @@ mod mock;
 mod tests;
 pub mod types;
 
+pub use types::*;
+
 #[frame_support::pallet]
 pub mod pallet {
+	use serde::ser;
+
 	use super::*;
 
 	/// The current storage version.
 	const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
 
-	#[pallet::config]
-	pub trait Config: frame_system::Config
-	where
-		<Self as frame_system::Config>::AccountId: From<H160>,
-	{
-		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-	}
-
-	#[pallet::error]
-	pub enum Error<T> {}
-
-	#[pallet::event]
-	#[pallet::generate_deposit(pub(crate) fn deposit_event)]
-	pub enum Event<T: Config>
-	where
-		<T as frame_system::Config>::AccountId: From<H160>, {}
-
 	#[pallet::pallet]
 	#[pallet::storage_version(STORAGE_VERSION)]
 	pub struct Pallet<T>(_);
 
-	#[pallet::hooks]
-	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> where
-		<T as frame_system::Config>::AccountId: From<H160>
-	{
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+		#[pallet::constant]
+		type MaxServiceEndpoints: Get<u32>;
+
+		#[pallet::constant]
+		type StringLimit: Get<u32>;
+
+		#[pallet::constant]
+		type ResolverMethod: Get<[u8; 9]>;
+	}
+
+	#[pallet::storage]
+	pub type Resolvers<T: Config> = StorageMap<
+		_,
+		Twox64Concat,
+		ResolverId<T::StringLimit>,
+		Resolver<T::AccountId, T::MaxServiceEndpoints, T::StringLimit>,
+	>;
+
+	#[pallet::error]
+	pub enum Error<T> {
+		/// The Resolver identifier is already in use
+		ResolverAlreadyRegistered,
+		/// The Resolver has not been registered
+		ResolverNotRegistered,
+		/// Account is not controller of resolver
+		NotController,
+	}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(crate) fn deposit_event)]
+	pub enum Event<T: Config> {
+		ResolverRegistered {
+			id: Vec<u8>,
+			controller: T::AccountId,
+			service_endpoints: BoundedVec<BoundedVec<u8, T::StringLimit>, T::MaxServiceEndpoints>,
+		},
+		ResolverUpdated {
+			id: Vec<u8>,
+			controller: T::AccountId,
+			service_endpoints: BoundedVec<BoundedVec<u8, T::StringLimit>, T::MaxServiceEndpoints>,
+		},
+		ResolverUnregistered {
+			id: Vec<u8>,
+		},
 	}
 
 	#[pallet::call]
-	impl<T: Config> Pallet<T> where <T as frame_system::Config>::AccountId: From<H160> {}
+	impl<T: Config> Pallet<T> {
+		#[pallet::call_index(0)]
+		#[pallet::weight(1_000)]
+		pub fn register_resolver(
+			origin: OriginFor<T>,
+			identifier: BoundedVec<u8, T::StringLimit>,
+			service_endpoints: BoundedVec<ServiceEndpoint<T::StringLimit>, T::MaxServiceEndpoints>,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+
+			let resolver_id = Self::get_reserved_resolver_id(identifier.clone());
+
+			ensure!(
+				<Resolvers<T>>::get(resolver_id.clone()).is_none(),
+				Error::<T>::ResolverAlreadyRegistered
+			);
+
+			let resolver =
+				Resolver { controller: who.clone(), service_endpoints: service_endpoints.clone() };
+
+			<Resolvers<T>>::insert(resolver_id, resolver);
+
+			Self::deposit_event(Event::ResolverRegistered {
+				id: identifier.to_vec(),
+				controller: who,
+				service_endpoints,
+			});
+
+			Ok(())
+		}
+
+		#[pallet::call_index(1)]
+		#[pallet::weight(1_000)]
+		pub fn update_resolver(
+			origin: OriginFor<T>,
+			identifier: BoundedVec<u8, T::StringLimit>,
+			service_endpoints: BoundedVec<ServiceEndpoint<T::StringLimit>, T::MaxServiceEndpoints>,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+
+			let resolver_id = Self::get_reserved_resolver_id(identifier.clone());
+			let mut resolver = <Resolvers<T>>::get(resolver_id.clone())
+				.ok_or(Error::<T>::ResolverNotRegistered)?;
+
+			ensure!(who == resolver.controller, Error::<T>::NotController);
+
+			resolver.service_endpoints = service_endpoints.clone();
+
+			<Resolvers<T>>::insert(resolver_id, resolver);
+
+			Self::deposit_event(Event::ResolverUpdated {
+				id: identifier.to_vec(),
+				controller: who,
+				service_endpoints,
+			});
+
+			Ok(())
+		}
+
+		#[pallet::call_index(2)]
+		#[pallet::weight(1_000)]
+		pub fn unregister_resolver(
+			origin: OriginFor<T>,
+			identifier: BoundedVec<u8, T::StringLimit>,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+
+			let resolver_id = Self::get_reserved_resolver_id(identifier.clone());
+			let mut resolver = <Resolvers<T>>::get(resolver_id.clone())
+				.ok_or(Error::<T>::ResolverNotRegistered)?;
+
+			ensure!(who == resolver.controller, Error::<T>::NotController);
+
+			<Resolvers<T>>::remove(resolver_id);
+
+			Self::deposit_event(Event::ResolverUnregistered { id: identifier.to_vec() });
+
+			Ok(())
+		}
+	}
+
+	impl<T: Config> Pallet<T> {
+		pub fn get_reserved_resolver_id(
+			identifier: BoundedVec<u8, T::StringLimit>,
+		) -> ResolverId<T::StringLimit> {
+			let method: BoundedVec<u8, T::StringLimit> =
+				BoundedVec::try_from(<T as Config>::ResolverMethod::get().to_vec())
+					.expect("Failed to convert invalid resolver method config");
+
+			ResolverId { method, identifier }
+		}
+	}
 }
