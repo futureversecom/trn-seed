7975ed8b3e931ed0dcfb6312ee8f1c861b6a7edf
z <zees-dev@users.noreply.github.com>
2025-01-24 09:58:55 +1300

    fixed compiler error for recursion limit

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -16,7 +16,7 @@
 //! Root runtime config
 #![cfg_attr(not(feature = "std"), no_std)]
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
-#![recursion_limit = "256"]
+#![recursion_limit = "512"]
 
 // Make the WASM binary available.
 #[cfg(feature = "std")]
