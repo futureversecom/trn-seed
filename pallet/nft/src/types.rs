ff1c87ca5dcfd9fdafcf5b5d7e2ba7bb2b094157
JasonT <50426640+JasonTulp@users.noreply.github.com>
2025-02-28 14:10:06 +1300

    Remove nft token sort (#930)
    * Remove sort
    
    * remove warning

--- pallet/nft/src/types.rs
+++ pallet/nft/src/types.rs
@@ -57,8 +57,7 @@ where
 		owner: AccountId,
 		serial_numbers: BoundedVec<SerialNumber, MaxTokensPerCollection>,
 	) -> Self {
-		let mut owned_serials = serial_numbers.clone();
-		owned_serials.sort();
+		let owned_serials = serial_numbers.clone();
 		Self { owner, owned_serials }
 	}
 
@@ -67,7 +66,6 @@ where
 		self.owned_serials
 			.try_push(serial_number)
 			.map_err(|_| TokenOwnershipError::TokenLimitExceeded)?;
-		self.owned_serials.sort();
 		Ok(())
 	}
 
