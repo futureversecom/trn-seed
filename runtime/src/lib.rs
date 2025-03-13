0aa0ed9a9fd139cf4482755b1d1d090e309c44c8
JasonT <50426640+JasonTulp@users.noreply.github.com>
2025-03-13 14:40:35 +1300

    Update spec version to 68 (#933)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -159,7 +159,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("root"),
 	impl_name: create_runtime_str!("root"),
 	authoring_version: 1,
-	spec_version: 67,
+	spec_version: 68,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 13,
