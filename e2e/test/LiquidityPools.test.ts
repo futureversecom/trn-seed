9d0f08f37b82b2207335685c1c5c62cdb0c76cbf
z <zees-dev@users.noreply.github.com>
2025-03-18 18:33:09 +1300

    liquidity pools e2e test

--- e2e/test/LiquidityPools.test.ts
+++ e2e/test/LiquidityPools.test.ts
@@ -2,13 +2,14 @@ import { ApiPromise, Keyring, WsProvider } from "@polkadot/api";
 import { KeyringPair } from "@polkadot/keyring/types";
 import { hexToU8a } from "@polkadot/util";
 import { expect } from "chai";
+import { Wallet } from "ethers";
 
 import {
   ALITH_PRIVATE_KEY,
   GAS_TOKEN_ID,
+  NATIVE_TOKEN_ID,
   NodeProcess,
   finalizeTx,
-  generateTestUsers,
   getNextAssetId,
   rpcs,
   sleep,
@@ -17,17 +18,14 @@ import {
 } from "../common";
 
 describe("Reward", () => {
-  let TOKEN_ID: number;
   let node: NodeProcess;
   let api: ApiPromise;
   let alith: KeyringPair;
-  let testUsers: KeyringPair[];
 
   before(async () => {
     node = await startNode();
 
-    const wsProvider = new WsProvider(`ws://localhost:${node.wsPort}`);
-    // const wsProvider = new WsProvider(`wss://archive.morel.micklelab.xyz/ws`);
+    const wsProvider = new WsProvider(`ws://localhost:${node.rpcPort}`);
     api = await ApiPromise.create({
       provider: wsProvider,
       types: typedefs,
@@ -36,51 +34,38 @@ describe("Reward", () => {
 
     const keyring = new Keyring({ type: "ethereum" });
     alith = keyring.addFromSeed(hexToU8a(ALITH_PRIVATE_KEY));
-
-    TOKEN_ID = await getNextAssetId(api);
-
-    // load test users
-    testUsers = generateTestUsers(10);
-
-    const txs = [
-      api.tx.assetsExt.createAsset("testliquiditypools", "TESTLIQUIDITYPOOLS", 6, 1, alith.address), // create asset
-    ];
-
-    await finalizeTx(alith, api.tx.utility.batch(txs));
   });
 
   after(async () => node.stop());
 
   it("load test for rollover", async () => {
-    const batchSize = Number(api.consts.liquidityPools.rolloverBatchSize.toHuman() as number) + 1;
-
-    const amount = 1000;
-    const joinPoolAmount = 100;
-
-    // fund test users
-    let txs = [];
-    for (let i = 0; i < testUsers.length; i++) {
-      const user = testUsers[i];
-      // transfer ROOT && GAS assets to test users
-      txs.push(api.tx.assets.mint(TOKEN_ID, user.address, amount));
-      txs.push(api.tx.assets.mint(GAS_TOKEN_ID, user.address, 100_000_000));
-      txs.push(api.tx.balances.transfer(user.address, amount));
-
-      if (txs.length >= batchSize || i === testUsers.length - 1) {
-        console.log(`funding ${txs.length} test users`);
-        await finalizeTx(alith, api.tx.utility.batch(txs));
-        txs = [];
-      }
-    }
-    console.log(`${testUsers.length} test users funded`);
-
-    const liquidityPoolsVaultAccount = "0x6d6f646c6c7164706f6f6c730000000000000000";
-    // fund vault account
+    const STAKED_TOKEN_ID = await getNextAssetId(api);
     await finalizeTx(
       alith,
-      api.tx.utility.batch([api.tx.balances.transfer(liquidityPoolsVaultAccount, 1_000_000_000 * 2)]),
+      api.tx.assetsExt.createAsset("testliquiditypools", "TESTLIQUIDITYPOOLS", 6, 1, alith.address),
     );
-    console.log(`vault account ${liquidityPoolsVaultAccount} funded`);
+
+    const BATCH_SIZE = Number(api.consts.liquidityPools.rolloverBatchSize.toHuman() as number) + 1;
+
+    const stake_amount = 1000;
+    const joinPoolAmount = 100;
+
+    // 10 users
+    const users = Array.from({ length: 10 }, () =>
+      new Keyring({ type: "ethereum" }).addFromSeed(hexToU8a(Wallet.createRandom().privateKey)),
+    );
+
+    // fund test users
+    const txs = [
+      // mint stake token
+      ...users.map((user) => api.tx.assets.mint(STAKED_TOKEN_ID, user.address, stake_amount)),
+      // mint gas token
+      ...users.map((user) => api.tx.assets.mint(GAS_TOKEN_ID, user.address, 100_000_000)),
+      // transfer root to test users
+      ...users.map((user) => api.tx.balances.transfer(user.address, stake_amount)),
+    ];
+    await finalizeTx(alith, api.tx.utility.batch(txs));
+    console.log(`${users.length} test users funded`);
 
     // pool 1
     const pool1 = await api.query.liquidityPools.nextPoolId();
@@ -89,30 +74,23 @@ describe("Reward", () => {
     const blockDuration = 4000;
     const intervalBlock = 5;
     let startBlock = intervalBlock + Number((await api.rpc.chain.getHeader()).number);
-    const rewardPeriod = Math.ceil(testUsers.length / batchSize) + intervalBlock;
+    const rewardPeriod = Math.ceil(users.length / BATCH_SIZE) + intervalBlock;
     let endBlock = startBlock + rewardPeriod;
 
     // create pool
     await finalizeTx(
       alith,
-      api.tx.sudo.sudo(api.tx.liquidityPools.createPool(TOKEN_ID, interestRate, maxTokens, startBlock, endBlock)),
+      api.tx.liquidityPools.createPool(NATIVE_TOKEN_ID, STAKED_TOKEN_ID, interestRate, maxTokens, startBlock, endBlock),
     );
     console.log(`pool ${pool1} created`);
 
-    // join pool
-    for (let i = 0; i < testUsers.length; i++) {
-      const user = testUsers[i];
-      api.tx.liquidityPools.enterPool(pool1, joinPoolAmount).signAndSend(user);
-
-      if (i % batchSize === 0 || i === testUsers.length - 1) {
-        await sleep(blockDuration);
-      }
-    }
-    console.log(`${testUsers.length} test users joined pool ${pool1}`);
+    // all users join pool
+    await Promise.all(users.map((user) => finalizeTx(user, api.tx.liquidityPools.enterPool(pool1, joinPoolAmount))));
+    console.log(`${users.length} test users joined pool ${pool1}`);
 
     // verify pool info's lock amount
     const pool1Info = ((await api.query.liquidityPools.pools(pool1)) as any).unwrap();
-    expect(pool1Info.lockedAmount.toNumber()).to.equal(testUsers.length * joinPoolAmount);
+    expect(pool1Info.lockedAmount.toNumber()).to.equal(users.length * joinPoolAmount);
     console.log(`pool ${pool1} locked amount verified`);
 
     // get next pool id
@@ -123,58 +101,58 @@ describe("Reward", () => {
     // create pool
     await finalizeTx(
       alith,
-      api.tx.sudo.sudo(api.tx.liquidityPools.createPool(TOKEN_ID, interestRate, maxTokens, startBlock, endBlock)),
+      api.tx.liquidityPools.createPool(NATIVE_TOKEN_ID, STAKED_TOKEN_ID, interestRate, maxTokens, startBlock, endBlock),
     );
     console.log(`pool ${pool2} created`);
 
     // set successor pool
-    await finalizeTx(alith, api.tx.sudo.sudo(api.tx.liquidityPools.setPoolSuccession(pool1, pool2)));
+    await finalizeTx(alith, api.tx.liquidityPools.setPoolSuccession(pool1, pool2));
     console.log(`pool ${pool2} set as successor of pool ${pool1}`);
 
+    // set pool rollover preference for users
+    await Promise.all(users.map((user) => finalizeTx(user, api.tx.liquidityPools.setPoolRollover(pool1, true))));
+    console.log(`${users.length} test users set pool rollover preference for pool ${pool2}`);
+
+    console.log(`waiting for start block ${startBlock}...`);
     await sleep(intervalBlock * 2 * blockDuration);
     console.log(`waited for start block ${startBlock}`);
 
     // wait for reward period
+    console.log(`waiting for reward period ${rewardPeriod} blocks...`);
     await sleep(rewardPeriod * blockDuration);
     console.log(`waited for reward period ${rewardPeriod} blocks`);
 
     // verify successor pool's lock amount
     const pool2Info = ((await api.query.liquidityPools.pools(pool2)) as any).unwrap();
-    expect(pool2Info.lockedAmount.toNumber()).to.equal(testUsers.length * joinPoolAmount);
+    expect(pool2Info.lockedAmount.toNumber()).to.equal(users.length * joinPoolAmount); // TODO: fix
     console.log(`pool ${pool2} locked amount verified`);
 
     // record user balances before claiming reward
-    const userBalancesBefore: { [key: string]: number } = {};
-    for (const user of testUsers) {
-      const userBalance: any = (await api.query.system.account(user.address)).toJSON();
-      userBalancesBefore[user.address] = userBalance?.data?.free;
-    }
+    const userBalancesBefore = await Promise.all(
+      users.map(async (user) => {
+        const userBalance: any = (await api.query.system.account(user.address)).toJSON();
+        return userBalance?.data?.free;
+      }),
+    );
 
     // claim reward for pool 1 & 2
-    for (let i = 0; i < testUsers.length; i++) {
-      const user = testUsers[i];
-      api.tx.liquidityPools.claimReward(pool1).signAndSend(user);
+    await Promise.all(users.map((user) => finalizeTx(user, api.tx.liquidityPools.claimReward(pool1))));
+    console.log(`${users.length} test users claimed reward for pool ${pool1}`);
 
-      if (i % batchSize === 0 || i === testUsers.length - 1) {
-        await sleep(blockDuration);
-      }
-    }
-    console.log(`${testUsers.length} test users claimed reward for pool ${pool1}`);
-    for (let i = 0; i < testUsers.length; i++) {
-      const user = testUsers[i];
-      api.tx.liquidityPools.claimReward(pool2).signAndSend(user);
+    await Promise.all(users.map((user) => finalizeTx(user, api.tx.liquidityPools.claimReward(pool2))));
+    console.log(`${users.length} test users claimed reward for pool ${pool2}`);
 
-      if (i % batchSize === 0 || i === testUsers.length - 1) {
-        await sleep(blockDuration);
-      }
-    }
-    console.log(`${testUsers.length} test users claimed reward for pool ${pool2}`);
+    // verify reward
+    const userBalancesAfter = await Promise.all(
+      users.map(async (user) => {
+        const userBalance: any = (await api.query.system.account(user.address)).toJSON();
+        return userBalance?.data?.free;
+      }),
+    );
 
-    for (const user of testUsers) {
-      // verify reward
-      const userBalance: any = (await api.query.system.account(user.address)).toJSON();
-      expect(userBalance?.data?.free).to.equal(joinPoolAmount * 2 + userBalancesBefore[user.address]);
+    for (let i = 0; i < users.length; i++) {
+      expect(userBalancesAfter[i]).to.equal(joinPoolAmount * 2 + userBalancesBefore[i]);
     }
-    console.log(`test users' reward verified`);
+    console.log(`test users reward verified`);
   });
 });
