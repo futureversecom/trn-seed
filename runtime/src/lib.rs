9e9afcc6addd7daea0564b91d293b55727f3f08c
JasonT <50426640+JasonTulp@users.noreply.github.com>
2025-06-17 15:48:29 +1200

    Release 11.75.0 (#952)
    * Update Spec Version to 75
    
    * Update Transaction version to 17

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -159,10 +159,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("root"),
 	impl_name: create_runtime_str!("root"),
 	authoring_version: 1,
-	spec_version: 74,
+	spec_version: 75,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 16,
+	transaction_version: 17,
 	state_version: 0,
 };
 
