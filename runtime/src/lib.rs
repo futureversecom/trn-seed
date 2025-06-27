94d0b420b828d0e191665ffa6e826d2dbc135df3
surangap <19677661+surangap@users.noreply.github.com>
2025-06-27 19:25:16 +0800

    Update runtime

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1429,6 +1429,7 @@ impl pallet_vortex_distribution::Config for Runtime {
 	type HistoryDepth = HistoryDepth;
 	type GasAssetId = GasAssetId;
 	type PartnerAttributionProvider = PartnerAttribution;
+	type MaxAttributionPartners = MaxAttributionPartners;
 }
 
 impl pallet_partner_attribution::Config for Runtime {
