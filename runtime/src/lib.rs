b188811cfbbbf7dac8e217e7dbf947bde6eee19a
John Carlo San Pedro <j.carlosanpedro@gmail.com>
2025-07-01 09:02:10 +1200

    Update spec version to 77 and transaction version to 18

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -159,10 +159,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("root"),
 	impl_name: create_runtime_str!("root"),
 	authoring_version: 1,
-	spec_version: 76,
+	spec_version: 77,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 17,
+	transaction_version: 18,
 	state_version: 0,
 };
 
