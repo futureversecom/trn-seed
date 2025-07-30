275cc6278ba15cd9e2d28b229d02cd879cf86a9a
JasonT <50426640+JasonTulp@users.noreply.github.com>
2025-07-30 12:50:10 +1200

    Release 11.80.0 (#963)
    * Update spec version to 80
    
    * Update transaction version
    
    * Update transaction version and fee control pallet version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -159,10 +159,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("root"),
 	impl_name: create_runtime_str!("root"),
 	authoring_version: 1,
-	spec_version: 79,
+	spec_version: 80,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 18,
+	transaction_version: 19,
 	state_version: 0,
 };
 
